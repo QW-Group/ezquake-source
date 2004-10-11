ca65709cbb06b254a5e2873eb8c773ff20543a52
vvd0 <vvd0@users.sourceforge.net>
2004-10-11 19:29:25 +0000

    Use dynamic determination of LITTLE or BIG ENDIAN (variable bigendien instead define LITTLE_ENDIAN)

--- sha1.h
+++ sha1.h
@@ -31,12 +31,16 @@ void SHA1Final(unsigned char digest[DIGEST_SIZE], SHA1_CTX* context);
 
 /* blk0() and blk() perform the initial expand. */
 /* I got the idea of expanding during the round function from SSLeay */
+/* Moved to sha1.c as function inline int blk0(int i); for use non 
+ * statical determination of LITTLE/BIG ENDIAN
 #ifdef LITTLE_ENDIAN
 #define blk0(i) (block->l[i] = (rol(block->l[i],24)&0xFF00FF00) \
     |(rol(block->l[i],8)&0x00FF00FF))
 #else
 #define blk0(i) block->l[i]
 #endif
+*/
+int blk0(int i);
 #define blk(i) (block->l[i&15] = rol(block->l[(i+13)&15]^block->l[(i+8)&15] \
     ^block->l[(i+2)&15]^block->l[i&15],1))
 
