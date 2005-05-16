08d4f6373bc7f9416fb63e55bd8249952cedff5d
johnnycz <johnnycz@users.sourceforge.net>
2005-05-16 07:40:09 +0000

    Cokeman: new HUD elements mp3_title and mp3_time

--- common_draw.h
+++ common_draw.h
@@ -12,6 +12,6 @@ void SCR_DrawBigClock(int x, int y, int style, int blink);
 void SCR_DrawSmallClock(int x, int y, int style, int blink);
 void SCR_NetStats(int x, int y, float period);
 void SCR_DrawSpeed2 (int x, int y, int type);
-
+void SCR_DrawWordWrapString(int x, int y, int y_spacing, int width, int height, int wordwrap, int scroll, double scroll_delay, char *txt);
 
 #endif  // __COMMON_GRAPH__H__
