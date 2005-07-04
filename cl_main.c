81e3f112d12fdaad4bd9424064e53ab468a22676
disconn3ct <disconn3ct@users.sourceforge.net>
2005-07-04 18:38:11 +0000

    '#include teamplay.h' moved to quakedef.h

--- cl_main.c
+++ cl_main.c
@@ -29,7 +29,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "sbar.h"
 #include "sound.h"
 #include "version.h"
-#include "teamplay.h"
 #include "image.h"
 
 #include "movie.h"
