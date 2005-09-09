f7bcab60e5313361f062ca506be23af666448de8
disconn3ct <disconn3ct@users.sourceforge.net>
2005-09-09 12:50:29 +0000

    sound.h --> qsound.h (had to rename since it conflicts with <Sound.h> Carbon(mac stuff) include)

--- ezquake/qsound.h
+++ ezquake/qsound.h
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-// sound.h -- client sound i/o functions
+// qsound.h -- client sound i/o functions
 
 #ifndef __SOUND__
 #define __SOUND__
