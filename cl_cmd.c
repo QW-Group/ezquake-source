8f0462b886be5bb524b7a9d66e604a8e1aa2a77b
disconn3ct <disconn3ct@users.sourceforge.net>
2005-10-19 15:13:46 +0000

    removed unused stuff. already at sys_win.c

--- cl_cmd.c
+++ cl_cmd.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 #include "winquake.h"
 #include "sha1.h"
-#include <io.h>
+// #include <io.h>
 
 #include "config_manager.h"
 
