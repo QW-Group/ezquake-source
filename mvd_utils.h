b2d02eb6467f713481a9cd90eab5990da8c1b2c8
johnnycz <johnnycz@users.sourceforge.net>
2005-11-23 23:12:16 +0000

    jogihoogi: mvd: powerup cams

--- mvd_utils.h
+++ mvd_utils.h
@@ -1,3 +1,6 @@
 void MVD_Screen (void);
 void MVD_Utils_Init (void); 
 void MVD_Mainhook_f (void);
+
+int powerup_cam_active,cam_1,cam_2,cam_3,cam_4;
+cvar_t mvd_pc_view_1,mvd_pc_view_2,mvd_pc_view_3,mvd_pc_view_4;
