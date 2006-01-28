180b4111d6bc8877c8e94b9636414fd0c6fd6e4d
johnnycz <johnnycz@users.sourceforge.net>
2006-01-28 16:36:16 +0000

    new HUD element gameclock; added scale property to both clock and gameclock HUD elements

--- ezquake/common_draw.h
+++ ezquake/common_draw.h
@@ -8,8 +8,8 @@ void CommonDraw_Init(void);
 //void PrepareCrosshair(int num, byte tab[10][10]);
 //void SCR_DrawClients(void);
 
-void SCR_DrawBigClock(int x, int y, int style, int blink);
-void SCR_DrawSmallClock(int x, int y, int style, int blink);
+void SCR_DrawBigClock(int x, int y, int style, int blink, float scale, int gametime);
+void SCR_DrawSmallClock(int x, int y, int style, int blink, float scale, int gametime);
 void SCR_NetStats(int x, int y, float period);
 void SCR_DrawSpeed2 (int x, int y, int type);
 void SCR_DrawWordWrapString(int x, int y, int y_spacing, int width, int height, int wordwrap, int scroll, double scroll_delay, char *txt);
