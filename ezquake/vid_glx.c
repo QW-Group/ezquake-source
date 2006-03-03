87bef52798388b7edddde8490c944cb308d060b1
disconn3ct <disconn3ct@users.sourceforge.net>
2006-03-03 19:50:14 +0000

    made gcc happy

--- ezquake/vid_glx.c
+++ ezquake/vid_glx.c
@@ -24,8 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdarg.h>
 #include <stdio.h>
 #include <signal.h>
-
-
+#include <unistd.h>
 
 #include "quakedef.h"
 
