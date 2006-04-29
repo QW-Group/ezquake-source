96a3a7deef526381a473d831ed6d5a4db8d401ba
johnnycz <johnnycz@users.sourceforge.net>
2006-04-29 17:34:19 +0000

    fixed newlines

--- image.c
+++ image.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	$Id: image.c,v 1.20 2006-04-08 13:15:01 disconn3ct Exp $
+	$Id: image.c,v 1.21 2006-04-29 17:34:19 johnnycz Exp $
 */
 
 #include "quakedef.h"
