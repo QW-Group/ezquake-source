1b37a3d0f0dfa959cae37d4fd8dfb7a34f4ca437
disconn3ct <disconn3ct@users.sourceforge.net>
2006-05-16 11:51:48 +0000

    fir win32 build after "net_udp.c + net_wins.c = net.c"

--- mdfour.h
+++ mdfour.h
@@ -1,32 +1,24 @@
 /*
-	mdfour.h
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-	an implementation of MD4 designed for use in the SMB authentication
-	protocol
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-	Copyright (C) Andrew Tridgell 1997-1998
+See the GNU General Public License for more details.
 
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-	See the GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to:
-
-		Free Software Foundation, Inc.
-		59 Temple Place - Suite 330
-		Boston, MA  02111-1307, USA
+    $Id: mdfour.h,v 1.3 2006-05-16 11:51:48 disconn3ct Exp $
 */
 
-#ifndef _MDFOUR_H
-#define _MDFOUR_H
+#ifndef __MDFOUR_H__
+#define __MDFOUR_H__
 
 #ifndef int32
 #define int32 int
@@ -50,5 +42,5 @@ void mdfour_update(struct mdfour *md, unsigned char *in, int n); //old: MD4Updat
 void mdfour_result(struct mdfour *md, unsigned char *out); // old: MD4Final
 void mdfour(unsigned char *out, unsigned char *in, int n);
 
-#endif	// _MDFOUR_H
+#endif /* __MDFOUR_H__ */
 
