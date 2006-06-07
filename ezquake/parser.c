b733fc8d022d4608a37951275c839d31e37fb002
oldmanuk <oldmanuk@users.sourceforge.net>
2006-06-07 20:13:07 +0000

    cleanup error codes

--- ezquake/parser.c
+++ ezquake/parser.c
@@ -1,5 +1,5 @@
 /*
-	$Id: parser.c,v 1.8 2006-06-07 18:26:38 oldmanuk Exp $
+	$Id: parser.c,v 1.9 2006-06-07 20:13:07 oldmanuk Exp $
 */
 
 #include "common.h"
@@ -10,50 +10,51 @@
 //#define DEBUG
 //#define MALLOC
 
-// returns -1 on error
-// returns 1 on no error
+// returns 0 on no error
+// returns 1 on error
 int Remove_Spaces (char *src){
 	int j = 0;
 	int i = 0;
 #ifdef MALLOC
 	char *msg;
-	msg=malloc(strlen(src));
+	msg = malloc(strlen(src));
 #else
 	char msg[1024];
 #endif
 	if (strlen(src)>SRCLIMIT)
-		return -1;
+		return 1;
 
-	strcpy(msg,src);
-	while (msg[i] != '\0' && i < strlen(src)){
-		if (msg[i] != ' '){
-			src[j++] = msg[i++];
-		}else
+	strcpy(msg, src);
+	while (msg[i] != '\0' && i < strlen(src)) {
+		if (msg[i] == ' '){
 			i++;
-
+		} else {
+			src[j++] = msg[i++];
+		}
 	}
 	src[j]='\0';
 #ifdef MALLOC
 	free(msg);
 #endif
-	return 1;
+
+	return 0;
 }
 
 
 // Checks for unwanted chars in the src string
 /*
 	returns
-	 1: no error
-	-1: found an invalid char in src
+	 0: no error
+	 1: found an invalid char in src
 */
 int Check_Tokens (char *src){
-	if (strspn(src,"1234567890-+*()") == strlen(src))
+	if (strspn(src,"1234567890-+*()") != strlen(src))
 		return 1;
-	else
-		return -1;
+
+	return 0;
 }
 
-// checks the acmount of ( and ) brackets and returns -1 if they dont match
+// checks the amount of ( and ) brackets and returns -1 if they dont match
 /* returns
 	 x: amount of () brackets
 	-1: non matching amounts of ('s and )'s
@@ -61,6 +62,7 @@ int Check_Tokens (char *src){
 int Check_Brackets (char *src){
 	int count_open,count_close;
 	int i;
+
 	count_open = 0;
 	count_close = 0;
 	for(i=0;i<strlen(src);i++){
@@ -69,18 +71,19 @@ int Check_Brackets (char *src){
 		if (src[i] == ')')
 			count_close++;
 	}
-	if (!(count_close - count_open))
-		return count_close;
-	else
+
+	if (count_close != count_open)
 		return -1;
+
+	return count_close;
 }
 
 
 // Checks for double of false tokens ++ -- +- etc
 /*
 	returns
-	 1: no errors
-	-1: error while copying the src string
+	 0: no errors
+	 1: error while copying the src string
 */
 int Check_For_Double_Tokens (char *src){
 #ifdef MALLOC
@@ -96,7 +99,7 @@ int Check_For_Double_Tokens (char *src){
 	int amount = 2;
 
 	if(strlen(src)>SRCLIMIT)
-		return -1;
+		return 1;
 
 #ifdef MALLOC
 	msg=malloc(strlen(src));
@@ -185,11 +188,9 @@ int Check_For_Double_Tokens (char *src){
 #ifdef MALLOC
 	free(msg);
 #endif
-	return 1;
+	return 0;
 
 }
-
-
 
 // works LOL :>
 int Calc_AB (char type,int a, int b){
@@ -212,9 +213,9 @@ int Calc_AB (char type,int a, int b){
 // will search the innermost bracket and send the content to Solve_String
 /*
 	returns
-	 1: no error
-	-1: error while copying the src string
-	-2: error in Check_Brackets
+	 0: no error
+	 1: error while copying the src string
+	 2: error in Check_Brackets
 */
 int Solve_Brackets (char *src){
 #ifdef MALLOC
@@ -228,28 +229,29 @@ int Solve_Brackets (char *src){
 	int z = 0;
 
 	if (strlen(src)>SRCLIMIT)
-		return -1;
+		return 1;
 #ifdef MALLOC
 	msg=malloc(strlen(src));
 #endif
 
 	strcpy(msg,src);
 
-	status=Check_Brackets(src);
-	if (status==-1){
+	if (Check_Brackets(src) == -1) {
 #ifdef MALLOC
 		free(msg);
 #endif
-		return -2;
-		}
-	for (z=0;z<status;z++){
-		while(src[i++]!='('){
+		return 2;
+	}
+
+	for (z=0;z<status;z++) {
+		while(src[i++]!='(') {
 		}
 	}
 	bracket_start=i;
 #ifdef MALLOC
 	bracket=malloc(1);
 #endif
+
 	while(src[i] !=')'){
 #ifdef MALLOC
 		realloc(bracket,sizeof(bracket)+1);
@@ -260,14 +262,13 @@ int Solve_Brackets (char *src){
 	bracket_stop = i;
 	bracket[y]='\0';
 
-	if(bracket_start){
+	if(bracket_start) {
 #ifdef MALLOC
 		part1=malloc(bracket_start-1);
 #endif
 		strncpy(part1,msg,bracket_start-1);
 		part1[bracket_start-1]='\0';
 	}
-
 
 	if(strlen(msg) != bracket_stop+1){
 #ifdef MALLOC
@@ -295,17 +296,16 @@ int Solve_Brackets (char *src){
 		free(part2);
 	}
 #endif
-	return 1;
-
+	return 0;
 }
 
 
 /*
 returns
-	0: if there is nothing more to do with the same token,
-	1: if there is still smth to do with the same token,
-	2: if everything is solved in the sting
-   -1: an error while copinyg the src string
+	-2: if there is nothing more to do with the same token,
+	-1: if there is still smth to do with the same token,
+	 0: if everything is solved in the sting
+     1: an error while copying the src string
 */
 int Calc_String (char *src,char tok){
 #ifdef MALLOC
@@ -317,7 +317,6 @@ int Calc_String (char *src,char tok){
 	int i=0;
 	char char_tok[] 	= "*-+";
 	char num_tok[]		= "1234567890";
-
 
 	// check if src is bigger than msg
 	if(strlen(src)>SRCLIMIT)
@@ -467,18 +466,18 @@ int Calc_String (char *src,char tok){
 
 /*
 	returns:
-	 1: for no error
-	-1: error in Check_For_Double_Tokens
-	-2: error in Calc_String
+	 0: for no error
+	 1: error in Check_For_Double_Tokens
+	 2: error in Calc_String
 
 */
-int Solve_String (char *src){
-	int status =0 ;
+int Solve_String (char *src) {
+	int status = 0;
 
 	while(!status){
 		status = Check_For_Double_Tokens(src);
-		if (status==-1)
-			return -1;
+		if (status > 0)
+			return 1;
 #ifdef DEBUG
 		printf("* LOOP\n");
 #endif
@@ -486,16 +485,16 @@ int Solve_String (char *src){
 #ifdef DEBUG
 		printf("%s\n",src);
 #endif
-		if (status==-1)
-			return -2;
-		if (status==2)
-			return 1;
+		if (status > 0)
+			return 2;
+		if (status == 0)
+			return 0;
 	}
-	status =0 ;
+	status = 0;
 	while(!status){
 		status = Check_For_Double_Tokens(src);
-		if (status==-1)
-			return -1;
+		if (status > 0)
+			return 1;
 #ifdef DEBUG
 		printf("- LOOP\n");
 #endif
@@ -503,16 +502,16 @@ int Solve_String (char *src){
 #ifdef DEBUG
 		printf("%s\n",src);
 #endif
-		if (status==-1)
-			return -2;
-		if (status==2)
-			return 1;
+		if (status > 0)
+			return 2;
+		if (status == 0)
+			return 0;
 	}
-		status =0 ;
+	status = 0;
 	while(!status){
 		status = Check_For_Double_Tokens(src);
-		if (status==-1)
-			return -1;
+		if (status > 0)
+			return 1;
 #ifdef DEBUG
 		printf("+ LOOP\n");
 #endif
@@ -520,55 +519,50 @@ int Solve_String (char *src){
 #ifdef DEBUG
 		printf("%s\n",src);
 #endif
-		if (status==-1)
-			return -2;
-		if (status==2)
-			return 1;
+		if (status > 0)
+			return 2;
+		if (status == 0)
+			return 0;
 	}
-		status =0 ;
-	return -1;
+		status = 0;
+
+	return 1;
 }
 
 
 /*
 returns:
 	 0: if no error occured
-	-1:	error while removing spaces
-	-2: error while checking bracket count
-	-3: error while solcing the bracket
-	-4:	error during Solve_String (error in Check_For_Double_Tokens)
-	-5:	error during Solve_String (error in Calc_String)
-	-6: unrecognized chars in the src string
+	 1:	error while removing spaces
+	 2: unrecognized chars in the src string
+	 3: error while checking bracket count
+	 4: error while solving the bracket
+	 5:	error during Solve_String (error in Check_For_Double_Tokens)
+	 6:	error during Solve_String (error in Calc_String)
 */
 // oldman: did someone's five year old child code this method originally? its
 // horrible :)
 int eval_string_int (char *src){
 	int status, status2;
 
-	status = Remove_Spaces(src);
-	if (status == -1)
-		return -1;
+	if(Remove_Spaces(src) > 0)
+		return 1;
 
-	status = Check_Tokens(src);
-	if (status == -1)
-		return -6;
+	if(Check_Tokens(src) > 0)
+		return 2;
 
-	while ((status = Check_Brackets(src))) {
-		if (status > 0) {
-			status2 = Solve_Brackets(src);
-			if (status2 == -1)
-				return -3;
-		} else if (status == -1)  {
-			return -2;
-		}
-	}
+	if(Check_Brackets(src) > 0) {
+		if(Solve_Brackets(src) > 0)
+			return 4;
+	} else
+		return 3;
 
 	status = Solve_String(src);
-	if (status == -1)
-		return -4;
+	if (status == 1)
+		return 5;
 
-	if (status == -2)
-		return -5;
+	if (status == 2)
+		return 6;
 
 	return 0;
 }
