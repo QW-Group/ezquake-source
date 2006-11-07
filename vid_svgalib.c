1166adcb45699b1c5be539ac14930d6a2a9cd3d3
disconn3ct <disconn3ct@users.sourceforge.net>
2006-11-07 13:55:15 +0000

    small fix. ignore it.

--- vid_svgalib.c
+++ vid_svgalib.c
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdarg.h>
 #include <stdio.h>
 #include <signal.h>
-#include <asm/io.h>
+#include <sys/io.h>
 #include "vga.h"
 #include "vgakeyboard.h"
 #include "vgamouse.h"
