87d20ccf2dec2abb7e5f8ece36fe846ba72ab1a6
disconn3ct <disconn3ct@users.sourceforge.net>
2006-11-25 23:54:21 +0000

    new libs dir structure

--- ezquake/libs/windows-libs-download.bat
+++ ezquake/libs/windows-libs-download.bat
@@ -1,1 +1,1 @@
-wget -r -nd --accept=lib --level=1 http://ezquake.sourceforge.net/libs/windows-x86/
+wget -r -nd --accept=lib --level=1 http://ezquake.sourceforge.net/libs/windows-x86/ -P ./windows-x86
