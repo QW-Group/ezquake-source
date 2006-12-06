af7705866e5c5d20d0ac6408bd1c9360e8e5d966
cokeman1982 <cokeman1982@users.sourceforge.net>
2006-12-06 01:07:46 +0000

    Multiview mini-HUD changes: - Added two new variables   * cl_mvhudflip - Flips the drawing of the hud, so first item is last and so on...   * cl_mvhudvertical - Draws the hud vertically, nice if you have the hud positioned at the sides of the view. - Changed so that cl_mvhudpos accepts strings instead of numbers as input, so you can type "bottom left" (or "bl" for short) for instance, easier to understand than 1-7. - Added drawing of what weapons the player has.

--- cl_screen.h
+++ cl_screen.h
@@ -1,6 +1,8 @@
 /*
-	$Id: cl_screen.h,v 1.2 2006-03-20 13:51:26 vvd0 Exp $
+	$Id: cl_screen.h,v 1.3 2006-12-06 01:07:46 cokeman1982 Exp $
 */
 
 #define	ELEMENT_X_COORD(var)	((var##_x.value < 0) ? vid.width - strlen(str) * 8 + 8 * var##_x.value: 8 * var##_x.value)
 #define	ELEMENT_Y_COORD(var)	((var##_y.value < 0) ? vid.height - sb_lines + 8 * var##_y.value : 8 * var##_y.value)
+
+qbool SCR_OnChangeMVHudPos(cvar_t *var, char *newval);
