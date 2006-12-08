6d6e2bbb15eefead235e5b11bdb36768ae120701
johnnycz <johnnycz@users.sourceforge.net>
2006-12-08 10:19:46 +0000

    windows libs now get downloaded from different CVS module by running <cvsroot>/ezquake/libs/windows-x86/download.bat; requires wget application to be present in PATH system env. variable; libs are ignored in this module

--- ezquake/libs/windows-libs-download.bat
+++
@@ -1,1 +0,0 @@
-wget -r -nd --accept=lib --level=1 http://ezquake.sourceforge.net/libs/windows-x86/ -P ./windows-x86
---
+++ ezquake/libs/windows-x86/download.bat
@@ -0,0 +1,7 @@
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libexpat.lib
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libjpeg.lib
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libpcre.lib
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libpng.lib
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libtcl8.4.lib
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libz.lib
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/mgllt.lib
