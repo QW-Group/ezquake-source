f34cca5416fd86ddc2ffde6c9822ea3e9d0e090e
disconn3ct <disconn3ct@users.sourceforge.net>
2006-12-08 19:36:09 +0000

    linux-x86 libs

---
+++ ezquake/libs/linux-x86/download.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libexpat.a
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libjpeg.a
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libpcre.a
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libpng.a
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libtcl8.4.a
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libz.a
