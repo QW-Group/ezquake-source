f92afcc212b4af6efac34fe4a43ae91fc58fdb78
cokeman1982 <cokeman1982@users.sourceforge.net>
2006-12-31 05:15:40 +0000

    ctrl.c - Stumped about really weird bug, any ideas? :)

--- ezquake/Ctrl.c
+++ ezquake/Ctrl.c
@@ -1,4 +1,4 @@
-//    $Id: Ctrl.c,v 1.4 2006-12-31 00:45:00 cokeman1982 Exp $
+//    $Id: Ctrl.c,v 1.5 2006-12-31 05:15:40 cokeman1982 Exp $
 
 #include "quakedef.h"
 
@@ -26,7 +26,13 @@ void UI_Print (int cx, int cy, char *str, int red)
 
 void UI_Print_Center (int cx, int cy, int w, char *str, int red)
 {
-	UI_Print(cx + (w - 8 * strlen(str)) / 2, cy, str, red);
+	// UH!!! wtf.... if I do:
+	// UI_Print(cx + (w - 8 * strlen(str)) / 2, cy, str, red);
+	// The call to UI_Print will get the first argument all fucked up (-217309285) instead of 192 in one instance.
+	// making the line that should be drawn to dissapear?!?!? ...... If I moved the strlen(str) outside of the
+	// function call like below it works fine.
+	int text_length = strlen(str);
+	UI_Print(cx + (w - 8 * text_length) / 2, cy, str, red);
 }
 
 void UI_MakeLine(char *buf, int w)
