69ad7c7bd138b7eb1964e56a95ce7170880242b2
johnnycz <johnnycz@users.sourceforge.net>
2007-01-01 20:28:53 +0000

    minizip.lib is in cvs/libs/windows-x86

--- ezquake/libs/windows-x86/download.bat
+++ ezquake/libs/windows-x86/download.bat
@@ -5,3 +5,4 @@ wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libp
 wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libtcl8.4.lib
 wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/libz.lib
 wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/mgllt.lib
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/minizip.lib
