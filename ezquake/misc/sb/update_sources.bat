55bd057bdb9eed8d8955e4cb4496c6a84ef838e8
johnnycz <johnnycz@users.sourceforge.net>
2007-01-05 18:14:24 +0000

    quakeservers.net new sources & update

--- ezquake/misc/sb/update_sources.bat
+++ ezquake/misc/sb/update_sources.bat
@@ -1,8 +1,8 @@
-wget -O au-sv.txt http://www.quakeservers.net/lists/australia/servers.txt
-wget -O ctf.txt http://www.quakeservers.net/lists/ctf/servers.txt
-wget -O eu-sv.txt http://www.quakeservers.net/lists/europe/servers.txt
-wget -O global.txt http://www.quakeservers.net/lists/servers/servers.txt
-wget -O na-sv.txt http://www.quakeservers.net/lists/north_america/servers.txt
-wget -O qizmo.txt http://www.quakeservers.net/lists/qizmo/servers.txt
-wget -O sa-sv.txt http://www.quakeservers.net/lists/south_america/servers.txt
-wget -O tf.txt http://www.quakeservers.net/lists/tf/servers.txt
+wget http://www.quakeservers.net/lists/australia/au-sv.txt
+wget http://www.quakeservers.net/lists/ctf/ctf.txt
+wget http://www.quakeservers.net/lists/europe/eu-sv.txt
+wget http://www.quakeservers.net/lists/servers/global.txt
+wget http://www.quakeservers.net/lists/north_america/na-sv.txt
+wget http://www.quakeservers.net/lists/qizmo/qizmo.txt
+wget http://www.quakeservers.net/lists/south_america/sa-sv.txt
+wget http://www.quakeservers.net/lists/tf/tf.txt
