f40c25c88c335a4daaa3de1a29380501b97627be
johnnycz <johnnycz@users.sourceforge.net>
2007-02-18 16:06:15 +0000

    user can enter any frequency he wishes (god save us)

--- ezquake/menu_options.c
+++ ezquake/menu_options.c
@@ -13,7 +13,7 @@
 	made by:
 		johnnycz, Jan 2006
 	last edit:
-		$Id: menu_options.c,v 1.40 2007-02-18 15:25:21 johnnycz Exp $
+		$Id: menu_options.c,v 1.41 2007-02-18 16:06:15 johnnycz Exp $
 
 */
 
@@ -735,12 +735,13 @@ int CT_Opt_FPS_Key (int k, CTab_t *tab, CTabPage_t *page) {
 
 // these variables serve as a temporary storage for user selected settings
 // they get initialized with current settings when the page is showed
-int menu_video_res, menu_video_bpp, menu_video_freq, menu_video_fullscreen;
+int menu_video_res, menu_video_bpp, menu_video_fullscreen;
+cvar_t vid_menu_tempfreq;
 
 void VidRestartAction (void) { 
 	Cvar_SetValue(&r_mode, menu_video_res);
 	Cvar_SetValue(&r_colorbits, menu_video_bpp);
-	Cvar_SetValue(&r_displayRefresh, menu_video_freq);
+	Cvar_SetValue(&r_displayRefresh, vid_menu_tempfreq.value);
 	Cvar_SetValue(&r_fullscreen, menu_video_fullscreen);
 	Cbuf_AddText("vid_restart\n");
 }
@@ -749,13 +750,9 @@ void VidRestartAction (void) {
 const char* glmodes[] = { "320x240", "400x300", "512x384", "640x480", "800x600", "960x720", "1024x768", "1152x864", "1280x1024", "1600x1200", "2048x1536" };
 int glmodes_size = sizeof(glmodes) / sizeof(char*);
 
-int screen_frequencies[] = { 60, 70, 72, 75, 85, 100, 120, 140, 144, 150 };
-int frequencies_size = sizeof(screen_frequencies) / sizeof(int);
-
 const char* BitDepthRead(void) { return menu_video_bpp == 32 ? "32 bit" : menu_video_bpp == 16 ? "16 bit" : "use desktop settings"; }
 const char* ResolutionRead(void) { return glmodes[bound(0, menu_video_res, glmodes_size-1)]; }
 const char* FullScreenRead(void) { return menu_video_fullscreen ? "on" : "off"; }
-const char* FreqRead(void) { return va("%i Hz", menu_video_freq); }
 
 void ResolutionToggle(qbool back) {
 	if (back) menu_video_res--; else menu_video_res++;
@@ -777,26 +774,13 @@ void BitDepthToggle(qbool back) {
 	}
 }
 void FullScreenToggle(qbool back) { menu_video_fullscreen = menu_video_fullscreen ? 0 : 1; }
-void FreqToggle(qbool back) {
-	int curfreq = 0;
-	int i;
-	for (i = 0; i < frequencies_size; i++) {
-		if (screen_frequencies[i] == menu_video_freq) { 
-			curfreq = i;
-			break;
-		}
-	}
-	if (back) curfreq--; else curfreq++;
-	curfreq = bound(0, curfreq, frequencies_size - 1);
-	menu_video_freq = screen_frequencies[curfreq];
-}
 
 setting settvideo_arr[] = {
 	ADDSET_SEPARATOR("Screen settings"),
 	ADDSET_CUSTOM("Resolution", ResolutionRead, ResolutionToggle, "Change your screen resolution used within the game"),
 	ADDSET_CUSTOM("Bit depth", BitDepthRead, BitDepthToggle, "Choose 16bit or 32bit color mode for your screen"),
 	ADDSET_CUSTOM("Fullscreen", FullScreenRead, FullScreenToggle, "Toggle fullscreen and windowed mode"),
-	ADDSET_CUSTOM("Refresh frequency", FreqRead, FreqToggle, "Choose your display horizontal refresh frequency; make sure that the selected frequency is supported by your hardware!"),
+	ADDSET_STRING("Refresh frequency", vid_menu_tempfreq),
 	ADDSET_ACTION("Apply changes", VidRestartAction, "Restarts the rendered and applies selected resolution"),
 	ADDSET_SEPARATOR("Text layer settings"),
 	ADDSET_NUMBER("Width", r_conwidth, 320, 2048, 8),
@@ -846,7 +830,7 @@ void OnShow_SettVideo(void) {
 
 	menu_video_res = (int) r_mode.value;
 	menu_video_bpp = (int) r_colorbits.value;
-	menu_video_freq = (int) r_displayRefresh.value;
+	Cvar_SetValue(&vid_menu_tempfreq, r_displayRefresh.value);
 	menu_video_fullscreen = (int) r_fullscreen.value;
 
 #endif
