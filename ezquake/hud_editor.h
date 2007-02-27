834acb37a055394c535f01448143c6657c17b0f9
cokeman1982 <cokeman1982@users.sourceforge.net>
2007-02-27 05:34:07 +0000

    hud.c: - Added a check for HUD editor, if a hud element should be drawn or not (in align/place mode, the contents of the hud element won't be drawn, just a border around it)
    hud_common.c:
    - We must ALWAYS call HUD_PrepareDraw for HUD elements, even if we don't intend to draw them for some reason. Otherwise the HUD editor won't be able to "see" them (they'll appear offscreen). Fixed this for teamholdbar and teamholdinfo, there are probably a few more to be fixed also.
    
    hud_editor.c:
    - Added a bunch of macros for what mouse buttons are down to get less clutter :)
    - Added "grephandles" that shows you were a HUD element that has been moved offscreen is located... You can then click and drag that handle to retrieve the HUD element.
    - Added different "hud editor modes", atm there is normal, align and off. In align mode the contents of the HUD elements won't be drawn (except the one we're aligning and it's children), only a border around it.

--- ezquake/hud_editor.h
+++ ezquake/hud_editor.h
@@ -4,9 +4,12 @@
 
 	made by jogihoogi, Feb 2007
 	last edit:
-	$Id: hud_editor.h,v 1.4 2007-02-24 14:26:03 johnnycz Exp $
+	$Id: hud_editor.h,v 1.5 2007-02-27 05:33:16 cokeman1982 Exp $
 
 */
+
+#ifndef __HUD_EDITOR_H__
+#define __HUD_EDITOR_H__
 
 // hud editor drawing function
 void HUD_Editor_Draw(void);
@@ -16,3 +19,21 @@ void HUD_Editor_Init(void);
 
 // key press processing function
 void HUD_Editor_Key(int, int);
+
+//
+// Should this HUD element be fully drawn or not when in align mode
+// when using the HUD editor.  
+//
+qbool HUD_Editor_ConfirmDraw(hud_t *hud);
+
+typedef enum
+{
+	hud_editmode_off,
+	hud_editmode_align,
+	hud_editmode_normal
+} hud_editor_mode_t;
+
+extern hud_editor_mode_t	hud_editor_mode;
+extern hud_t				*selected_hud;
+
+#endif // __HUD_EDITOR_H__
