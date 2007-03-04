8db6015b10906ea6ceb6be8c015996a6a54824f6
disconn3ct <disconn3ct@users.sourceforge.net>
2007-03-04 19:14:05 +0000

    cleanup

--- bspfile.h
+++ bspfile.h
@@ -18,8 +18,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#ifndef __bspfile_h
-#define __bspfile_h
+#ifndef __BSPFILE_H__
+#define __BSPFILE_H__
 
 // upper design bounds
 
@@ -214,63 +214,4 @@ typedef struct
 	byte		ambient_level[NUM_AMBIENTS];
 } dleaf_t;
 
-//============================================================================
-
-#ifndef QUAKE_GAME
-
-// the utilities get to be lazy and just use large static arrays
-
-extern	int			nummodels;
-extern	dmodel_t	dmodels[MAX_MAP_MODELS];
-
-extern	int			visdatasize;
-extern	byte		dvisdata[MAX_MAP_VISIBILITY];
-
-extern	int			lightdatasize;
-extern	byte		dlightdata[MAX_MAP_LIGHTING];
-
-extern	int			texdatasize;
-extern	byte		dtexdata[MAX_MAP_MIPTEX]; // (dmiptexlump_t)
-
-extern	int			entdatasize;
-extern	char		dentdata[MAX_MAP_ENTSTRING];
-
-extern	int			numleafs;
-extern	dleaf_t		dleafs[MAX_MAP_LEAFS];
-
-extern	int			numplanes;
-extern	dplane_t	dplanes[MAX_MAP_PLANES];
-
-extern	int			numvertexes;
-extern	dvertex_t	dvertexes[MAX_MAP_VERTS];
-
-extern	int			numnodes;
-extern	dnode_t		dnodes[MAX_MAP_NODES];
-
-extern	int			numtexinfo;
-extern	texinfo_t	texinfo[MAX_MAP_TEXINFO];
-
-extern	int			numfaces;
-extern	dface_t		dfaces[MAX_MAP_FACES];
-
-extern	int			numclipnodes;
-extern	dclipnode_t	dclipnodes[MAX_MAP_CLIPNODES];
-
-extern	int			numedges;
-extern	dedge_t		dedges[MAX_MAP_EDGES];
-
-extern	int			nummarksurfaces;
-extern	unsigned short	dmarksurfaces[MAX_MAP_MARKSURFACES];
-
-extern	int			numsurfedges;
-extern	int			dsurfedges[MAX_MAP_SURFEDGES];
-
-
-
-void	LoadBSPFile (char *filename);
-void	WriteBSPFile (char *filename);
-void	PrintBSPFileSizes (void);
-
-#endif
-
-#endif // __bspfile_h
+#endif /* !__BSPFILE_H__ */
--- mathlib.c
+++ mathlib.c
@@ -16,16 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	$Id: mathlib.c,v 1.5 2007-03-01 23:35:09 cokeman1982 Exp $
+	$Id: mathlib.c,v 1.6 2007-03-04 19:14:05 disconn3ct Exp $
 
 */
 
 #include "common.h"
-#ifdef GLQUAKE
-#include "gl_model.h"	//for struct mplane_s
-#else
-#include "r_model.h"	//for struct mplane_s
-#endif
+struct mplane_s;
 
 vec3_t vec3_origin = {0, 0, 0};
 
