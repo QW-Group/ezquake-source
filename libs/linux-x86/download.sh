8233bdc83ee47dc49fe7cbfa004d6efc0475ccb1
disconn3ct <disconn3ct@users.sourceforge.net>
2007-03-11 00:13:44 +0000

    add minizip.lib

--- libs/linux-x86/download.sh
+++ libs/linux-x86/download.sh
@@ -5,3 +5,4 @@ wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libpcr
 wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libpng.a
 wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libtcl8.4.a
 wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/libz.a
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86/minizip.a
