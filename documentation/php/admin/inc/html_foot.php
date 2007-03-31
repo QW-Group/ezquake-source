514c565c57a38fc1e3fdab392c6013af6563b405
no author <***@nowhere>
2007-03-31 23:49:47 +0000

    This commit was manufactured by cvs2svn to create branch 'ezquake_18'.

--- documentation/php/admin/inc/html_foot.php
+++
@@ -1,7 +0,0 @@
-<address><a href="http://validator.w3.org/check?uri=referer">XHTML 1.0 Strict</a> |
-<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS 2.0</a> |
-<a href="http://www.php.net/">PHP</a> | <a href="http://www.mysql.com/">MySQL</a> | 
-<a href="http://www.gnu.org/copyleft/gpl.html">GNU/GPL</a> |
-made by <a href="http://sourceforge.net/users/johnnycz/">JohnNy_cz</a></address>
-</body>
-</html>
--- documentation/php/admin/inc/menu_headadmin.php
+++
@@ -1,8 +0,0 @@
-<div class="menu">
-<h2>Headadmin</h2>
-<ul class="mainmenu">
-<li><a href="?action=adduserform">Add User</a></li>
-<li><a href="?action=edituserform">Edit User</a></li>
-<li><a href="?action=removeuserform">Remove User</a></li>
-</ul>
-</div>
