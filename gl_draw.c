7d0f4a31f89a7ca715051fbb5e0be0f65190e351
johnnycz <johnnycz@users.sourceforge.net>
2007-04-10 15:21:17 +0000

    fixed newlines (I got "0D0D0A" as a newline after cvs update in windows)

--- gl_draw.c
+++ gl_draw.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	$Id: gl_draw.c,v 1.59 2007-04-10 06:02:43 qqshka Exp $
+	$Id: gl_draw.c,v 1.60 2007-04-10 15:21:17 johnnycz Exp $
 */
 
 #include "quakedef.h"
