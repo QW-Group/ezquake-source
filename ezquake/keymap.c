1c97561c002925d4200da4030088d042c1e3e522
johnnycz <johnnycz@users.sourceforge.net>
2007-05-20 09:09:35 +0000

    in_builtinkeymap back to 1, it is a hard decision to make, but we prefer not to confuse existing users; it will be safe to turn this to 0 in the next release because they can store this default into their configs

--- ezquake/keymap.c
+++ ezquake/keymap.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	$Id: keymap.c,v 1.10 2007-01-12 11:37:03 oldmanuk Exp $
+	$Id: keymap.c,v 1.10.2.1 2007-05-20 09:09:35 johnnycz Exp $
 
 */
 // keymap.c -- support for international keyboard layouts
@@ -29,7 +29,7 @@ extern cvar_t cl_keypad;
 
 // name of the keymapping:
 cvar_t	keymap_name = {"keymap_name", "Default"};
-cvar_t	in_builtinkeymap = {"in_builtinkeymap", "0"};
+cvar_t	in_builtinkeymap = {"in_builtinkeymap", "1"};
 
 // flag, which shows if a keymapping is active or not:
 static qbool keymap_active = false; 
