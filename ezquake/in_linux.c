2acb74ca251196ee6c06a34cdaf79ed6bb011021
disconn3ct <disconn3ct@users.sourceforge.net>
2007-05-27 15:35:22 +0000

    1) fix cursor_sensitivity for linux 2) -nodga == -nomdga, -mevdev -mmt and -nomouse command line switches are supported by linux builds.

--- ezquake/in_linux.c
+++ ezquake/in_linux.c
@@ -14,10 +14,12 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	$Id: in_linux.c,v 1.7 2007-03-15 18:26:59 qqshka Exp $
+	$Id: in_linux.c,v 1.7.2.1 2007-05-27 15:35:22 disconn3ct Exp $
 */
+
 #include "quakedef.h"
 #include "input.h"
+#include "keys.h"
 
 #ifdef NDEBUG
 #define win_mouse	"1"
@@ -59,45 +61,59 @@ void IN_MouseMove (usercmd_t *cmd)
 		; // FIXME: is there was missed ; or it was ok ?
 #endif
 
-	if (m_filter.value) {
-		float filterfrac = bound (0.0f, m_filter.value, 1.0f) / 2.0f;
-		mouse_x = (mx * (1.0f - filterfrac) + old_mouse_x * filterfrac);
-		mouse_y = (my * (1.0f - filterfrac) + old_mouse_y * filterfrac);
-	} else {
-		mouse_x = mx;
-		mouse_y = my;
-	}
-
-	old_mouse_x = mx;
-	old_mouse_y = my;
-
-	if (m_accel.value) {
-		float mousespeed = (sqrt (mx * mx + my * my)) / (1000.0f * (float) cls.trueframetime);
-		mouse_x *= (mousespeed * m_accel.value + sensitivity.value);
-		mouse_y *= (mousespeed * m_accel.value + sensitivity.value);
-	} else {
-		mouse_x *= sensitivity.value;
-		mouse_y *= sensitivity.value;
-	}
-
-	// add mouse X/Y movement to cmd
-	if ((in_strafe.state & 1) || (lookstrafe.value && mlook_active))
-		cmd->sidemove += m_side.value * mouse_x;
-	else
-		cl.viewangles[YAW] -= m_yaw.value * mouse_x;
-
-	if (mlook_active)
-		V_StopPitchDrift ();
-
-	if (mlook_active && !(in_strafe.state & 1))
+	//
+	// Do not move the player if we're in HUD editor or menu mode.
+	// And don't apply ingame sensitivity, since that will make movements jerky.
+	//
+	if(key_dest == key_hudeditor || key_dest == key_menu)
 	{
-		cl.viewangles[PITCH] += m_pitch.value * mouse_y;
-		if (cl.viewangles[PITCH] > cl.maxpitch)
-			cl.viewangles[PITCH] = cl.maxpitch;
-		if (cl.viewangles[PITCH] < cl.minpitch)
-			cl.viewangles[PITCH] = cl.minpitch;
-	} else {
-		cmd->forwardmove -= m_forward.value * mouse_y;
+		old_mouse_x = mouse_x = mx * cursor_sensitivity.value;
+		old_mouse_y = mouse_y = my * cursor_sensitivity.value;
+	}
+	else
+	{
+		// Normal game mode.
+
+		if (m_filter.value) {
+			float filterfrac = bound (0.0f, m_filter.value, 1.0f) / 2.0f;
+			mouse_x = (mx * (1.0f - filterfrac) + old_mouse_x * filterfrac);
+			mouse_y = (my * (1.0f - filterfrac) + old_mouse_y * filterfrac);
+		} else {
+			mouse_x = mx;
+			mouse_y = my;
+		}
+
+		old_mouse_x = mx;
+		old_mouse_y = my;
+
+		if (m_accel.value) {
+			float mousespeed = (sqrt (mx * mx + my * my)) / (1000.0f * (float) cls.trueframetime);
+			mouse_x *= (mousespeed * m_accel.value + sensitivity.value);
+			mouse_y *= (mousespeed * m_accel.value + sensitivity.value);
+		} else {
+			mouse_x *= sensitivity.value;
+			mouse_y *= sensitivity.value;
+		}
+
+		// add mouse X/Y movement to cmd
+		if ((in_strafe.state & 1) || (lookstrafe.value && mlook_active))
+			cmd->sidemove += m_side.value * mouse_x;
+		else
+			cl.viewangles[YAW] -= m_yaw.value * mouse_x;
+
+		if (mlook_active)
+			V_StopPitchDrift ();
+
+		if (mlook_active && !(in_strafe.state & 1))
+		{
+			cl.viewangles[PITCH] += m_pitch.value * mouse_y;
+			if (cl.viewangles[PITCH] > cl.maxpitch)
+				cl.viewangles[PITCH] = cl.maxpitch;
+			if (cl.viewangles[PITCH] < cl.minpitch)
+				cl.viewangles[PITCH] = cl.minpitch;
+		} else {
+			cmd->forwardmove -= m_forward.value * mouse_y;
+		}
 	}
 
 	mx = my = 0; // clear for next update
@@ -105,23 +121,44 @@ void IN_MouseMove (usercmd_t *cmd)
 
 void IN_Move (usercmd_t *cmd)
 {
-	IN_MouseMove(cmd);
+	IN_MouseMove (cmd);
 }
 
 void IN_Init (void)
 {
-	Cvar_SetCurrentGroup(CVAR_GROUP_INPUT_MOUSE);
-	Cvar_Register(&m_filter);
+	int i = 0;
+
+	Cvar_SetCurrentGroup (CVAR_GROUP_INPUT_MOUSE);
+	Cvar_Register (&m_filter);
 #ifndef _Soft_SVGA
-	Cvar_Register(&_windowed_mouse);
+	Cvar_Register (&_windowed_mouse);
 #endif
 
-	Cvar_SetCurrentGroup(CVAR_GROUP_INPUT_KEYBOARD);
-	Cvar_Register(&cl_keypad);
-	Cvar_ResetCurrentGroup();
+	Cvar_SetCurrentGroup (CVAR_GROUP_INPUT_KEYBOARD);
+	Cvar_Register (&cl_keypad);
+	Cvar_ResetCurrentGroup ();
 
 	if (!host_initialized)
 	{
+		typedef enum { mt_none = 0, mt_dga, mt_normal, mt_evdev } mousetype_t;
+		extern cvar_t in_mouse;
+		extern cvar_t in_mmt;
+		extern cvar_t in_evdevice;
+
+		if (COM_CheckParm ("-nodga") || COM_CheckParm ("-nomdga"))
+			Cvar_LatchedSetValue (&in_mouse, mt_normal);
+
+		if (i == COM_CheckParm ("-mevdev") && (i < com_argc - 1)) {
+			Cvar_LatchedSet (&in_evdevice, com_argv[i + 1]);
+			Cvar_LatchedSetValue (&in_mouse, mt_evdev);
+		}
+
+		if (COM_CheckParm ("-mmt"))
+			Cvar_LatchedSetValue (&in_mmt, 1);
+
+		if (COM_CheckParm ("-nomouse"))
+			Cvar_LatchedSetValue (&in_mouse, mt_none);
+
 #ifdef WITH_EVDEV
 		extern void IN_EvdevList_f(void);
 		Cmd_AddCommand ("in_evdevlist", IN_EvdevList_f);
@@ -134,10 +171,7 @@ void IN_Init (void)
 #endif
 	}
 
-	if (!COM_CheckParm ("-nomouse"))
-		IN_StartupMouse();
-	else
-    mouseinitialized = false;
+	IN_StartupMouse ();
 }
 
 void IN_Shutdown(void)
