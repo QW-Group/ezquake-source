fe557cfadb1b871fd25b8032422a99d87674a33b
himan <himan@users.sourceforge.net>
2007-06-17 01:44:32 +0000

    gfxinfo -> vid_gfxinfo for consistency

--- tr_init.c
+++ tr_init.c
@@ -19,7 +19,7 @@ along with Foobar; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 
-	$Id: tr_init.c,v 1.18 2007-06-15 21:07:12 himan Exp $
+	$Id: tr_init.c,v 1.19 2007-06-17 01:44:32 himan Exp $
 
 */
 // tr_init.c -- functions that are not called every frame
@@ -557,7 +557,7 @@ void R_Register( void )
 		Cmd_AddCommand( "vid_showfreq",	VID_ShowFreq_f );
 #endif
 		Cmd_AddCommand( "vid_modelist",		R_ModeList_f );
-		Cmd_AddCommand( "gfxinfo",		GfxInfo_f );
+		Cmd_AddCommand( "vid_gfxinfo",		GfxInfo_f );
 		Cmd_AddCommand( "vid_restart",	VID_Restart_f );
 	}
 }
