07a7ff48f7573918ea8240a10a9040eb284d3f2a
disconn3ct <disconn3ct@users.sourceforge.net>
2007-06-17 16:31:55 +0000

    update mouse data every frame, not only physical frame. it affects DGA, and X11 mouse. windows alreyad update data every frame

--- cl_main.c
+++ cl_main.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-$Id: cl_main.c,v 1.148 2007-06-15 12:26:05 johnnycz Exp $
+$Id: cl_main.c,v 1.149 2007-06-17 16:31:55 disconn3ct Exp $
 */
 // cl_main.c  -- client main loop
 
@@ -1617,6 +1617,7 @@ void CL_Frame (double time) {
 				|| cls.state == ca_disconnected // We need to move the mouse also when disconnected 
 				) {
 				usercmd_t dummy;
+				Sys_SendKeyEvents();
 				IN_Move (&dummy);
 			}
 		}
