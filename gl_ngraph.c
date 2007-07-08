f3468bfb3bd2bc42c7368459adce86561021cadc
cokeman1982 <cokeman1982@users.sourceforge.net>
2007-07-08 15:22:44 +0000

    clamp is defined in mathlib.h, instead of 4 different files :)

--- gl_ngraph.c
+++ gl_ngraph.c
@@ -25,9 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "hud.h"
 #include "hud_common.h"
 
-
-#define clamp(a,b,c) (a = min(max(a, b), c))
-
 int	netgraphtexture;	// netgraph texture
 
 // HUD -> hexum
