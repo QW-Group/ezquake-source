0bf5acb8bb6366fd20e7b5a954174602f283a403
disconn3ct <disconn3ct@users.sourceforge.net>
2007-07-20 02:53:30 +0000

    tweaks to MSVC8 project

--- masm32/download.bat
+++ masm32/download.bat
@@ -1,1 +1,2 @@
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/ML.EXE
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/ml.exe
+wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/gas2masm.exe
