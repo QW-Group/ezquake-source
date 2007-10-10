20ce17176be41bfecbd451fc4089dfd9447daabf
dkure <dkure@users.sourceforge.net>
2007-10-10 17:38:19 +0000

    Added full gzip support with the assumption that a gzip file is not contained within any other file (ie fopen, fread and fwrite, fseek etc word like normal)

--- vfs_gzip.c
+++ vfs_gzip.c
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *     
- * $Id: vfs_gzip.c,v 1.4 2007-09-29 18:48:48 dkure Exp $
+ * $Id: vfs_gzip.c,v 1.5 2007-10-10 17:38:19 dkure Exp $
  *             
  */
 
@@ -32,25 +32,47 @@
 //=============================================================================
 //                       G Z I P   V F S
 //=============================================================================
-typedef struct {
+typedef struct gzipfile_s
+{
+	char filename[MAX_QPATH];
+	vfsfile_t *handle;
+
+	packfile_t file; // Only one file can be stored in a gzip file
+
+	unsigned long filepos;
+
+	int references;
+} gzipfile_t;
+
+
+typedef struct 
+{
 	vfsfile_t funcs; // <= must be at top/begining of struct
 
-	gzFile handle;
+	gzipfile_t *parent;
+
+	unsigned long startpos;
+	unsigned long length;
+	unsigned long currentpos;
 } vfsgzipfile_t;
 
 // FIXME:
 // Everything below assumes that the input file was an OS file
 // This may not be the case if we are opening a gz file in a gz file...
-
-int VFSGZIP_ReadBytes(vfsfile_t *file, void *buffer, int bytestoread, vfserrno_t *err) 
+// The assumption is due to the minizip library not having hooks for
+// f* operations (fread, fwrite, fseek etc)
+//
+// This assumption may be possible to over come by using streams instead
+//
+static int VFSGZIP_ReadBytes(vfsfile_t *file, void *buffer, int bytestoread, vfserrno_t *err) 
 {
 	int r;
-	vfsgzipfile_t *intfile = (vfsgzipfile_t *)file;
+	vfsgzipfile_t *vfsgz = (vfsgzipfile_t *)file;
 
 	if (bytestoread < 0)
-		        Sys_Error("VFSOS_ReadBytes: bytestoread < 0");
+		        Sys_Error("VFSGZIP_ReadBytes: bytestoread < 0");
 	
-	r = gzread(intfile->handle, buffer, bytestoread);
+	r = gzread(vfsgz->parent->handle, buffer, bytestoread);
 	// r == -1 on error
 
 	if (err) // if bytestoread <= 0 it will be treated as non error even we read zero bytes
@@ -60,91 +82,245 @@ int VFSGZIP_ReadBytes(vfsfile_t *file, void *buffer, int bytestoread, vfserrno_t
 	return r;
 }
 
-int VFSGZIP_WriteBytes(vfsfile_t *file, const void *buffer, int bytestowrite) 
+static int VFSGZIP_WriteBytes(vfsfile_t *file, const void *buffer, int bytestowrite) 
 {
 	int r;
-	vfsgzipfile_t *intfile = (vfsgzipfile_t *)file;
+	vfsgzipfile_t *vfsgz = (vfsgzipfile_t *)file;
 
-	r = gzwrite(intfile->handle, buffer, bytestowrite);
+	r = gzwrite(vfsgz->parent->handle, buffer, bytestowrite);
 
 	// r == 0 on error
 	
 	return r;
 }
 
-qbool VFSGZIP_Seek(vfsfile_t *file, unsigned long offset, int whence) 
+static qbool VFSGZIP_Seek(vfsfile_t *file, unsigned long offset, int whence) 
 {
 	int r;
-	vfsgzipfile_t *intfile = (vfsgzipfile_t *)file;
+	vfsgzipfile_t *vfsgz = (vfsgzipfile_t *)file;
 
-	r = gzseek(intfile->handle, offset, whence);
+	r = gzseek(vfsgz->parent->handle, offset, whence);
 	return r;
 }
 
-unsigned long VFSGZIP_Tell(vfsfile_t *file) 
+static unsigned long VFSGZIP_Tell(vfsfile_t *file) 
 {
 	int r;
-	vfsgzipfile_t *intfile = (vfsgzipfile_t *)file;
+	vfsgzipfile_t *vfsgz = (vfsgzipfile_t *)file;
 
-	r = gztell(intfile->handle);
+	r = gztell(vfsgz->parent->handle);
 	return r;
 }
 
-unsigned long VFSGZIP_GetLen(vfsfile_t *file) 
+static unsigned long VFSGZIP_GetLen(vfsfile_t *file) 
 {
 	int r, currentpos;
-	vfsgzipfile_t *intfile = (vfsgzipfile_t *)file;
+	vfsgzipfile_t *vfsgz = (vfsgzipfile_t *)file;
 	
 	// VFS-FIXME: Error handling
-	currentpos = gztell(intfile->handle);
-	r = gzseek(intfile->handle, 0, SEEK_END);
-	gzseek(intfile->handle, currentpos, SEEK_SET);
+	currentpos = gztell(vfsgz->parent->handle);
+	r = gzseek(vfsgz->parent->handle, 0, SEEK_END);
+	gzseek(vfsgz->parent->handle, currentpos, SEEK_SET);
 
 	return r;
 }
 
-void VFSGZIP_Close(vfsfile_t *file) 
+static void VFSGZIP_Close(vfsfile_t *file) 
 {
 	int r;
-	vfsgzipfile_t *intfile = (vfsgzipfile_t *)file;
+	vfsgzipfile_t *vfsgz = (vfsgzipfile_t *)file;
 
-	r = gzclose(intfile->handle);
+	r = gzclose(vfsgz->parent->handle);
 }
 
-void VFSGZIP_Flush(vfsfile_t *file) 
+static void VFSGZIP_Flush(vfsfile_t *file) 
 {
 	int r;
-	vfsgzipfile_t *intfile = (vfsgzipfile_t *)file;
+	vfsgzipfile_t *vfsgz = (vfsgzipfile_t *)file;
 
-	//r = gzflush(intfile->handle, Z_NO_FLUSH); 	// <-- Allows better compression
-	r = gzflush(intfile->handle, Z_SYNC_FLUSH); 	// <-- All pending output is flushed
+	//r = gzflush(vfsgz->parent->handle, Z_NO_FLUSH); 	// <-- Allows better compression
+	r = gzflush(vfsgz->parent->handle, Z_SYNC_FLUSH); 	// <-- All pending output is flushed
 }
 
-// TODO: Should add a IO mode (read/write) for the function
-vfsfile_t *VFSGZIP_Open(vfsfile_t *file, char *desc) 
+static vfsfile_t *FSGZIP_OpenVFS(void *handle, flocation_t *loc, char *mode) 
 {
-	vfsosfile_t *intfile = (vfsosfile_t*)file; // FIXME: <-- ASSUMTPTION!
-	vfsgzipfile_t *gzipfile = Q_calloc(1, sizeof(*gzipfile));
+	gzipfile_t *gzip = (gzipfile_t *)handle;
+	vfsgzipfile_t *vfsgz;
 
-	int fd = fileno(intfile->handle);
-
-	gzipfile->handle = gzdopen(dup(fd), "r");
-	if (gzipfile->handle == NULL) {
-		free(gzipfile);
+	if (strcmp(mode, "rb"))
 		return NULL;
-	}
+
+	vfsgz = Q_calloc(1, sizeof(*vfsgz));
+	vfsgz->parent = gzip;
+	gzip->references++;
+
+	vfsgz->startpos   = loc->offset;
+	vfsgz->length     = loc->len;
+	vfsgz->currentpos = vfsgz->startpos;
 
 	// TODO: Use mode to determine NULL read/write VFS calls
-	gzipfile->funcs.ReadBytes  = VFSGZIP_ReadBytes;
-	gzipfile->funcs.WriteBytes = VFSGZIP_WriteBytes;
-	gzipfile->funcs.Seek       = VFSGZIP_Seek;
-	gzipfile->funcs.Tell       = VFSGZIP_Tell;
-	gzipfile->funcs.GetLen     = VFSGZIP_GetLen;
-	gzipfile->funcs.Close      = VFSGZIP_Close;
-	gzipfile->funcs.Flush      = VFSGZIP_Flush;
+	vfsgz->funcs.ReadBytes  = VFSGZIP_ReadBytes;
+	vfsgz->funcs.WriteBytes = NULL; // VFSGZIP_WriteBytes;
+	vfsgz->funcs.Seek       = VFSGZIP_Seek;
+	vfsgz->funcs.Tell       = VFSGZIP_Tell;
+	vfsgz->funcs.GetLen     = VFSGZIP_GetLen;
+	vfsgz->funcs.Close      = VFSGZIP_Close;
+	vfsgz->funcs.Flush      = VFSGZIP_Flush;
 
-	return (vfsfile_t *)gzipfile;
+	return (vfsfile_t *)vfsgz;
 }
+
+
+//=============================================
+// GZIP file  (*.gz) - Search Functions
+//=============================================
+static void FSGZIP_PrintPath(void *handle)
+{
+	gzipfile_t *gzip = (gzipfile_t *)handle;
+
+	if (gzip->references != 1)
+		Com_Printf("%s (%i)\n", gzip->filename, gzip->references-1);
+	else
+		Com_Printf("%s\n", gzip->filename);
+}
+
+static void FSGZIP_ClosePath(void *handle)
+{
+	gzipfile_t *gzip = (gzipfile_t *)handle;
+
+	if (--gzip->references > 0)
+		return; //not yet time
+
+	VFS_CLOSE(gzip->handle);
+	Q_free(gzip);
+}
+
+static void FSGZIP_BuildHash(void *handle)
+{
+	gzipfile_t *gzip = (gzipfile_t *)handle;
+
+	if (!Hash_GetInsensitive(filesystemhash, gzip->file.name))
+	{
+		Hash_AddInsensitive(filesystemhash, gzip->file.name, &gzip->file);
+		fs_hash_files++;
+	}
+	else {
+		fs_hash_dups++;
+	}
+}
+
+static qbool FSGZIP_FLocate(void *handle, flocation_t *loc, const char *filename, void *hashedresult)
+{
+	packfile_t *pf = hashedresult;
+	gzipfile_t *gzip = (gzipfile_t *)handle;
+
+	// look through all the pak file elements
+
+	//is this a pointer to a file in this pak?
+	if (pf && pf != &(gzip->file)) {
+		return false;   //was found in a different path
+	} else if (!strcmp (gzip->file.name, filename)) {
+		pf = &gzip->file;
+	}
+
+	if (pf)
+	{
+		if (loc)
+		{
+			loc->index = 0;
+			snprintf(loc->rawname, sizeof(loc->rawname), "%s/%s", gzip->filename, filename);
+			loc->offset = pf->filepos;
+			loc->len = pf->filelen;
+		}
+		return true;
+	}
+	return false;
+
+}
+
+static void FSGZIP_ReadFile(void *handle, flocation_t *loc, char *buffer)
+{
+	gzipfile_t *gzip = handle;
+	int err;
+
+	VFS_SEEK(gzip->handle, gzip->file.filepos, SEEK_SET);
+
+	err = VFS_READ(gzip->handle, buffer, gzip->file.filelen, NULL);
+
+	if (err!=gzip->file.filelen)
+	{
+		Com_Printf ("Can't extract file \"%s:%s\" (corrupt)\n", gzip->filename, gzip->file.name);
+		return;
+	}
+	return;
+}
+
+static int FSGZIP_EnumerateFiles (void *handle, char *match, int (*func)(char *, int, void *), void *parm)
+{
+	gzipfile_t *gzip = handle;
+
+	if (wildcmp(match, gzip->file.name))
+	{
+		if (!func(gzip->file.name, gzip->file.filelen, parm))
+			return false;
+	}
+
+	return true;
+}
+
+// =================
+// FSTAR_LoadGZipFile
+// =================
+// Takes an explicit (not game tree related) path to a gzip file.
+//
+// Loads the header and directory, adding the files at the beginning
+// of the list so they override previous pack files.
+
+static void *FSGZIP_LoadGZipFile(vfsfile_t *gziphandle, char *desc)
+{
+	gzipfile_t *gzip;
+	const char *base;
+	char *ext;
+
+	gzip = Q_calloc(1, sizeof(*gzip));
+	strlcpy(gzip->filename, desc, sizeof(gzip->filename));
+	if (gziphandle == NULL) goto fail;
+
+	int fd = fileno(((vfsosfile_t *)gziphandle)->handle); // <-- ASSUMPTION! that file is OS
+	gzip->handle = gzdopen(dup(fd), "r");
+	gzip->references = 1;
+
+	/* Remove the .gz from the file.name */
+	base = COM_SkipPath(desc);
+	ext = COM_FileExtension(desc);
+	if (strcmp(ext, "gz") == 0) {
+		COM_StripExtension(base, gzip->file.name);
+	} else {
+		strlcpy(gzip->file.name, base, sizeof(gzip->file.name));
+	}
+
+
+	Com_Printf("Successfully opened %s, 1 file added\n", desc);
+	return gzip;
+
+fail:
+	// Q_free is safe to call on NULL pointers
+	Q_free(gzip);
+	Com_Printf("Unable to open %s\n", desc);
+	return NULL;
+}
+
+searchpathfuncs_t gzipfilefuncs = {
+	FSGZIP_PrintPath,
+	FSGZIP_ClosePath,
+	FSGZIP_BuildHash,
+	FSGZIP_FLocate,
+	FSGZIP_ReadFile,
+	FSGZIP_EnumerateFiles,
+	FSGZIP_LoadGZipFile,
+	NULL, // VFS-FIXME: Might be used for f_modification
+	FSGZIP_OpenVFS 
+};
 
 #endif // WITH_VFS_GZIP
 #endif // WITH_ZLIB
