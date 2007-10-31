4585f28b4e582aa48e8666545a8ab3ef9ded6620
dkure <dkure@users.sourceforge.net>
2007-10-31 09:22:13 +0000

    Updated the library download scripts to use the new svn repository

--- libs/freebsd-x86/download.sh
+++ libs/freebsd-x86/download.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/freebsd-x86/libexpat.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/freebsd-x86/libjpeg.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/freebsd-x86/libpcre.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/freebsd-x86/libpng.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/freebsd-x86/libtcl8.4.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/freebsd-x86/libz.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/freebsd-x86/minizip.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libexpat.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libjpeg.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpcre.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpng.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libtcl8.4.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libz.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/minizip.a
--- libs/linux-x86_64/download.sh
+++ libs/linux-x86_64/download.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86_64/libexpat.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86_64/libjpeg.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86_64/libpcre.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86_64/libpng.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86_64/libtcl8.4.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86_64/libz.a
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/linux-x86_64/minizip.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libexpat.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libjpeg.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libpcre.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libpng.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libtcl8.4.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libz.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/minizip.a
