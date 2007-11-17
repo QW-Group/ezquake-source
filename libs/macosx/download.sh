de81a4f12747e0d9c5c7a3f9bbc1c94e0f071428
disconn3ct <disconn3ct@users.sourceforge.net>
2007-11-17 18:35:15 +0000

    Chaning SVN keywrods" *.c, *.h, *.s, gnu.txt,  - native Makefile - LF win32 projects, ezquake.exe.manifest, qasm.rules, winquake.rc - CRLF (masm32, libs)/<download scripts> - linux/bsd - LF, win32 - CRLF, macosx - CR (not sure) make - update svn:ignore misc/install - CRLF, misc/<rest> - native

--- libs/macosx/download.sh
+++ libs/macosx/download.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
-curl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libexpat.a
-curl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libjpeg.a
-curl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libpcre.a
-curl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libpng.a
-curl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libtcl8.4.a
-curl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libz.a
-curl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/minizip.a
+#!/bin/shcurl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libexpat.acurl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libjpeg.acurl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libpcre.acurl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libpng.acurl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libtcl8.4.acurl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/libz.acurl -O http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/macosx/minizip.a
