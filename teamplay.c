05da953f7f8cb6c2897d731c6334de39ce182be5
zwoch <zwoch@users.sourceforge.net>
2007-11-27 18:53:18 +0000

    #include <limits.h> for UINT_MAX usage. If it breaks archs make it #ifdef __APPLE__.

--- teamplay.c
+++ teamplay.c
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <time.h>
 #include <string.h>
+#include <limits.h>
 #include "quakedef.h"
 #include "ignore.h"
 #ifdef GLQUAKE
