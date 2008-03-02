a4622b64ed13be041725c5e08acd12f7ac309322
johnnycz <johnnycz@users.sourceforge.net>
2008-03-02 22:18:34 +0000

    killed a warning for non-gl builds

--- r_draw.c
+++ r_draw.c
@@ -32,6 +32,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "qsound.h"
 #include "image.h"
 #include "common_draw.h"
+#include "utils.h"
+
 
 typedef struct
 {
