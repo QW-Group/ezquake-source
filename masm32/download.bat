d9c8148b116e114e2e5e5df54b23a3923b839628
cokeman1982 <cokeman1982@users.sourceforge.net>
2008-08-12 21:24:01 +0000

    Changed the paths of the download.bats to use the wget in the repos instead.

--- masm32/download.bat
+++ masm32/download.bat
@@ -1,2 +1,2 @@
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/ML.EXE
-wget http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/gas2masm.exe
+"../make/wget.exe" http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/ML.EXE
+"../make/wget.exe" http://ezquake.cvs.sourceforge.net/*checkout*/ezquake/libs/windows-x86/gas2masm.exe
