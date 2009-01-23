a55394be24ed51b5a55abe5c070459b4f7f8ac08
bioentropy <bioentropy@users.sourceforge.net>
2009-01-23 03:31:40 +0000

    added missing irclib in download script for 64bit linux

--- libs/linux-x86_64/download.sh
+++ libs/linux-x86_64/download.sh
@@ -6,3 +6,4 @@ wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/lin
 wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libtcl.a
 wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libz.a
 wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/minizip.a
+wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libircclient.a
