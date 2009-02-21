7a6b12c0f2d98e909ea8413b437fec2294c488b5
vvd0 <vvd0@users.sourceforge.net>
2009-02-21 13:52:26 +0000

    Char "*" must be quoted in download.sh scripts (may be download.bat too)

--- libs/freebsd-x86/download.sh
+++ libs/freebsd-x86/download.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-#fetch -AapRr http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/as
-mlibe.a
-fetch -AapRr -S 157280 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libexpat.a
-fetch -AapRr -S 25952 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebs
-d-x86/libircclient.a
-fetch -AapRr -S 158976 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libjpeg.a
-fetch -AapRr -S 214038 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpcre.a
-fetch -AapRr -S 168692 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpng.a
-fetch -AapRr -S 1552626 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libtcl.a
-fetch -AapRr -S 78106 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libz.a
-fetch -AapRr -S 21120 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/minizip.a
+#fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/asmlibe.a'
+fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libexpat.a'
+fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libircclient.a'
+fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libjpeg.a'
+fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpcre.a'
+fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpng.a'
+fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libtcl.a'
+fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libz.a'
+fetch -aRr 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/minizip.a'
 
--- libs/linux-x86_64/download.sh
+++ libs/linux-x86_64/download.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libexpat.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libjpeg.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libpcre.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libpng.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libtcl.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libz.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/minizip.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libircclient.a
+wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libexpat.a'
+wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libircclient.a'
+wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libjpeg.a'
+wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libpcre.a'
+wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libpng.a'
+wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libtcl.a'
+wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/libz.a'
+wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86_64/minizip.a'
