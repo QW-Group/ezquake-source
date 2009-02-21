e36993927fb146a61ad3d4ab3718c6eb7305666b
vvd0 <vvd0@users.sourceforge.net>
2009-02-21 13:22:19 +0000

    FreeBSD use fetch command for fetch files - it included in base system

--- libs/freebsd-x86/download.sh
+++ libs/freebsd-x86/download.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libexpat.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libjpeg.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpcre.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpng.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libtcl.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libz.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/minizip.a
-#wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/asmlibe.a
-wget http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libircclient.a
+#fetch -AapRr http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/as
+mlibe.a
+fetch -AapRr -S 157280 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libexpat.a
+fetch -AapRr -S 25952 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebs
+d-x86/libircclient.a
+fetch -AapRr -S 158976 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libjpeg.a
+fetch -AapRr -S 214038 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpcre.a
+fetch -AapRr -S 168692 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libpng.a
+fetch -AapRr -S 1552626 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libtcl.a
+fetch -AapRr -S 78106 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/libz.a
+fetch -AapRr -S 21120 http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/freebsd-x86/minizip.a
+
