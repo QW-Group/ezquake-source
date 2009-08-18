9372b45243f029b6623a84c84cc4d62256f502b8
aas_at_sf_net <aas_at_sf_net@users.sourceforge.net>
2009-08-18 11:51:11 +0000

    Added windows-x86_64 library directory (something moving in the dark). Updated DirectX libraries (March 2009). Removed deprecated /OPT:NOWIN98 and unnecessary /MACHINE:I386 flags from 2008.vcproj file.

---
+++ libs/windows-x86_64/download.bat
@@ -0,0 +1,14 @@
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/libexpat.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/libjpeg.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/libpcre.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/libpng.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/libtcl.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/libz.lib
+@rem "../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/mgllt.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/minizip.lib
+@rem "../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/asmlibm.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/dinput.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/dsound.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/dxguid.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/libircclient-static.lib
+"../../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86_64/glew32.lib
