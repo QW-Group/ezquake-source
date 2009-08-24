93834c73391bbe809399b541fd1d4174fa992e5d
johnnycz <johnnycz@users.sourceforge.net>
2009-08-24 14:42:11 +0000

    version is 2.0 beta

--- version.h
+++ version.h
@@ -72,7 +72,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 
-#define VERSION_NUMBER "2.0 alpha"
+#define VERSION_NUMBER "2.0 beta"
 
 int build_number (void);
 void CL_Version_f (void);
