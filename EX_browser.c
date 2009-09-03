887f6f53d948c9c026e36a9ae9e18601e1f95774
johnnycz <johnnycz@users.sourceforge.net>
2009-09-03 19:01:03 +0000

    bugfix: autoupdate - crashed when entering multiplayer menu, credits to biomass

--- EX_browser.c
+++ EX_browser.c
@@ -757,6 +757,16 @@ void SB_Servers_OnShow (void)
 	static qbool updated = false;
 
 	if (sb_autoupdate.value && !updated) {
+		if (rebuild_servers_list)
+			Rebuild_Servers_List();
+
+		if (resort_servers)
+		{
+			Sort_Servers();
+			resort_servers = 0;
+		}
+
+
 		GetServerPingsAndInfos(true);
 		updated = true;
 	}
