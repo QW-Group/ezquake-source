bf0680d077ba89da36eefe8306a52629f760f005
aas_at_sf_net <aas_at_sf_net@users.sourceforge.net>
2009-09-09 09:01:33 +0000

    Updated: libraries download scripts for windows and linux, "compiling libs on unix" manual, linux-x86_64 libraries.

--- libs/linux-x86/download.sh
+++ libs/linux-x86/download.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libexpat.a'
-wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libircclient.a'
-wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libjpeg.a'
-wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libpcre.a'
-wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libpng.a'
-wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libtcl.a'
-wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libz.a'
-wget 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/minizip.a'
-
+wget -O 'libexpat.a' 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libexpat.a'
+wget -O 'libircclient.a' 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libircclient.a'
+wget -O 'libjpeg.a' 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libjpeg.a'
+wget -O 'libpcre.a' 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libpcre.a'
+wget -O 'libpng.a' 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libpng.a'
+wget -O 'libtcl.a' 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libtcl.a'
+wget -O 'libz.a' 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/libz.a'
+wget -O 'minizip.a' 'http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/linux-x86/minizip.a'
