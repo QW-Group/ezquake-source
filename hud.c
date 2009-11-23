f7659ea59670301aad628d15d623ab99be43f747
aas_at_sf_net <aas_at_sf_net@users.sourceforge.net>
2009-11-23 15:33:54 +0000

    * Changed default hud_*_item_opacity from 1.0 to 0.99 to make images with alpha look more smooth

--- hud.c
+++ hud.c
@@ -1289,7 +1289,7 @@ hud_t * HUD_Register(char *name, char *var_alias, char *description,
 	// Item Opacity.
 	//
 	{
-		hud->opacity = HUD_CreateVar(name, "item_opacity", (item_opacity) ? item_opacity : "1.0");
+		hud->opacity = HUD_CreateVar(name, "item_opacity", (item_opacity) ? item_opacity : "0.99");
 		hud->flags |= HUD_OPACITY;
 		hud->params[hud->num_params++] = hud->opacity;
 	}
