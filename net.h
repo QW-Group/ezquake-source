8b23335991c7dbdf023c7ecaf29fc2ebbd2075f3
qqshka <qqshka@users.sourceforge.net>
2010-01-24 21:29:35 +0000

    #include <ws2tcpip.h> required by plugins.c now... who knows why.

--- net.h
+++ net.h
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifdef _WIN32
 #include <winsock2.h>
+#include <ws2tcpip.h>
 typedef int socklen_t;
 typedef SOCKET socket_t;
 
