e2c9f95ebe73bbb8d598e333854921148475c619
johnnycz <johnnycz@users.sourceforge.net>
2010-02-26 17:22:06 +0000

    fix warning

--- gl_rmain.c
+++ gl_rmain.c
@@ -32,6 +32,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "hud_common.h"
 #include "gl_bloom.h"
 #include "rulesets.h"
+#include "teamplay.h"
+
 
 entity_t	r_worldentity;
 
