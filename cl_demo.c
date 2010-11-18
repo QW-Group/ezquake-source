21ce85f60f11422171afaaaa8da276c3a6198c64
Pekka Lampila <pekka.lampila@iki.fi>
2010-11-19 01:43:31 +0200

    Add a missing mvd_utils.h include for cl_demo.c

--- cl_demo.c
+++ cl_demo.c
@@ -43,6 +43,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "logging.h"
 #include "version.h"
 #include "demo_controls.h"
+#include "mvd_utils.h"
 #ifndef CLIENTONLY
 #include "server.h"
 #endif
