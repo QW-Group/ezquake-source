cc501f1dad744d08486804383cc07e471c977d37
Pekka Lampila <pekka.lampila@iki.fi>
2010-11-19 01:44:26 +0200

    Add stdlib.h include for cl_demo.c for strtoll

--- cl_demo.c
+++ cl_demo.c
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include <time.h>
+#include <stdlib.h>
 #include "quakedef.h"
 #include "winquake.h"
 #include "movie.h"
