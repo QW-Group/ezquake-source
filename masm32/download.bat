ed6b93a06c0f2e335afab94216e73b66a3022f64
Jan Raszyk <jan.raszyk@gmail.com>
2010-12-29 12:48:54 +0100

    fix: don't check certificates as windows wget fails badly on that

--- masm32/download.bat
+++ masm32/download.bat
@@ -1,2 +1,2 @@
-"../make/wget.exe" http://github.com/ezQuake/ezquake-libs/raw/master/windows-x86/ml.exe
-"../make/wget.exe" http://github.com/ezQuake/ezquake-libs/raw/master/windows-x86/gas2masm.exe
+"../make/wget.exe" --no-check-certificate http://github.com/ezQuake/ezquake-libs/raw/master/windows-x86/ml.exe
+"../make/wget.exe" --no-check-certificate http://github.com/ezQuake/ezquake-libs/raw/master/windows-x86/gas2masm.exe
