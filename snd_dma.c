72c6f54872043d68e36fb229b93b3bdca47db59f
qqshka <ivan.bolsunov@gmail.com>
2011-01-09 15:10:44 +0300

    fix: should be able to compile on linux now.

--- snd_dma.c
+++ snd_dma.c
@@ -973,7 +973,7 @@ typedef void *dllhandle_t;
 
 #ifdef _WIN32
 
-// well, its windows specific and should be in sys_win.c but ATM voicechat is windows only anyway, so...
+// well, its OS specific and should be in sys_xxx.c but...
 
 static void Sys_CloseLibrary(dllhandle_t *lib)
 {
@@ -992,6 +992,38 @@ static dllhandle_t *Sys_LoadLibrary(const char *name, dllfunction_t *funcs)
 	for (i = 0; funcs[i].name; i++)
 	{
 		*funcs[i].funcptr = GetProcAddress(lib, funcs[i].name);
+		if (!*funcs[i].funcptr)
+			break;
+	}
+	if (funcs[i].name)
+	{
+		Sys_CloseLibrary((dllhandle_t*)lib);
+		lib = NULL;
+	}
+
+	return (dllhandle_t*)lib;
+}
+#else // _WIN32
+
+#include <dlfcn.h>
+
+static void Sys_CloseLibrary(dllhandle_t *lib)
+{
+	dlclose((void*)lib);
+}
+
+static dllhandle_t *Sys_LoadLibrary(const char *name, dllfunction_t *funcs)
+{
+	int i;
+	dllhandle_t lib;
+
+	lib = dlopen (name, RTLD_LAZY);
+	if (!lib)
+		return NULL;
+
+	for (i = 0; funcs[i].name; i++)
+	{
+		*funcs[i].funcptr = dlsym(lib, funcs[i].name);
 		if (!*funcs[i].funcptr)
 			break;
 	}
