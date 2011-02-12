689042fd3dc05e2e7c5baf860e280b4592919e13
Jan Raszyk <jan.raszyk@gmail.com>
2011-02-12 14:38:07 +0100

    changed default of cfg_use_gamedir to 0
    probably this disables some behavior for those who want to have it on, not sure
    this cvar did bring too much frustration and confusion

--- config_manager.c
+++ config_manager.c
@@ -74,7 +74,7 @@ cvar_t	cfg_save_cmdline	=	{"cfg_save_cmdline", "1"};
 cvar_t	cfg_backup			=	{"cfg_backup", "0"};
 cvar_t	cfg_legacy_exec		=	{"cfg_legacy_exec", "1"};
 cvar_t  cfg_use_home		=	{"cfg_use_home", "0"};
-cvar_t  cfg_use_gamedir		=	{"cfg_use_gamedir", "1"};
+cvar_t  cfg_use_gamedir		=	{"cfg_use_gamedir", "0"};
 
 /************************************ DUMP FUNCTIONS ************************************/
 
