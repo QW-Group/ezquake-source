bafeee81404f44b2c495c251e19107191ccbe9c2
Jan Raszyk <jan.raszyk@gmail.com>
2011-02-19 23:47:33 +0100

    version is 2.1 beta

--- version.h
+++ version.h
@@ -72,7 +72,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 
-#define VERSION_NUMBER "2.1 alpha"
+#define VERSION_NUMBER "2.1 beta"
 
 int build_number (void);
 void CL_Version_f (void);
