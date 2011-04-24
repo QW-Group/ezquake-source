a627958cc2b04b73a212a2e837c6862b1485aa05
JohnNy_cz <jan.raszyk@gmail.com>
2011-04-24 21:26:54 +0200

    mingw libs download script

---
+++ libs/mingw-x86/download.sh
@@ -0,0 +1,14 @@
+wget --no-check-certificate -O libexpat.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/libexpat.lib
+wget --no-check-certificate -O libjpeg.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/libjpeg.lib
+wget --no-check-certificate -O libpcre.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/libpcre.lib
+wget --no-check-certificate -O libpng.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/libpng.lib
+wget --no-check-certificate -O libtcl.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/libtcl.lib
+wget --no-check-certificate -O libz.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/libz.lib
+wget --no-check-certificate -O mgllt.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/mgllt.lib
+wget --no-check-certificate -O minizip.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/minizip.lib
+wget --no-check-certificate -O dinput.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/dinput.lib
+wget --no-check-certificate -O dsound.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/dsound.lib
+wget --no-check-certificate -O dxguid.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/dxguid.lib
+wget --no-check-certificate -O libircclient-static.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/libircclient-static.lib
+wget --no-check-certificate -O glew32.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/glew32.lib
+wget --no-check-certificate -O libcurl.lib http://github.com/ezQuake/ezquake-libs/raw/master/mingw-x86/libcurl.lib
