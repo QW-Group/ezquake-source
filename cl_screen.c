3c472f7c5b550b7f17f63ffb04d79214930a2e62
JohnNy_cz <jan.raszyk@gmail.com>
2011-11-01 21:25:53 +0100

    fix compilation in msvs
    <insert rant about microsoft not supporting c99>

--- cl_screen.c
+++ cl_screen.c
@@ -1694,7 +1694,7 @@ static int SCR_Draw_TeamInfoPlayer(ti_player_t *ti_cl, int x, int y, int maxname
 						}
 						else {
 							char *weap_str = TP_ItemName(BestWeaponFromStatItems( ti_cl->items ));
-							size_t buf_size = strlen(weap_str) + 1;
+							size_t buf_size = 32;
 							char weap_white_stripped[buf_size];
 							Util_SkipChars(weap_str, "{}", weap_white_stripped, buf_size);
 							Draw_ColoredString (x, y, weap_white_stripped, false);
