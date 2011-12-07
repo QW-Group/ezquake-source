c872754bb8c0a11bd987b8430bfc33a08d5d5874
JohnNy_cz <jan.raszyk@gmail.com>
2011-12-07 22:49:02 +0100

    version is 2.1.1 stable
    this version contains only a hotfix for the silly crash bug
    + ability to compile with msvs2010

--- version.h
+++ version.h
@@ -72,7 +72,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 
-#define VERSION_NUMBER "2.1 stable"
+#define VERSION_NUMBER "2.1.1 stable"
 
 int build_number (void);
 void CL_Version_f (void);
