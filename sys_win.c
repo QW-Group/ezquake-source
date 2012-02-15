30876d5e2fb0c0d2bd5684565b36ec7895a7f914
JohnNy_cz <jan.raszyk@gmail.com>
2012-02-15 16:18:18 +0100

    fix DEP issue properly, suggested by Spike

--- sys_win.c
+++ sys_win.c
@@ -385,7 +385,7 @@ void Sys_MakeCodeWriteable (unsigned long startaddr, unsigned long length)
 	DWORD  flOldProtect;
 
 	//@@@ copy on write or just read-write?
-	if (!VirtualProtect((LPVOID)startaddr, length, PAGE_READWRITE, &flOldProtect))
+	if (!VirtualProtect((LPVOID)startaddr, length, PAGE_EXECUTE_READWRITE, &flOldProtect))
    		Sys_Error("Protection change failed");
 }
 
