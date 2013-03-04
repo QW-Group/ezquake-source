762db4bdf7b5f40b1f71f672c02f41bf08d96e30
dimman <fog@udden.net>
2013-03-04 22:49:17 +0100

    Add STAT_MATCHSTARTTIME to support new gameclock sync

--- common.h
+++ common.h
@@ -42,76 +42,70 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //============================================================================
 
 // per-level limits
-#define	CL_MAX_EDICTS		2048	// FIXME: ouch! ouch! ouch!
-//#define	SV_MAX_EDICTS		1024	// FIXME: ouch! ouch! ouch!
-#define	MAX_EDICTS			512		// FIXME: ouch! ouch! ouch! - trying to fix...
-#define	MAX_LIGHTSTYLES		64
-#define	MAX_MODELS			512		// these are sent over the net as bytes
-#define MAX_VWEP_MODELS 	32		// could be increased to 256
-#define	MAX_SOUNDS			256		// so they cannot be blindly increased
+#define CL_MAX_EDICTS           2048	// FIXME: ouch! ouch! ouch!
+//#define SV_MAX_EDICTS         1024	// FIXME: ouch! ouch! ouch!
+#define MAX_EDICTS              512	// FIXME: ouch! ouch! ouch! - trying to fix...
+#define MAX_LIGHTSTYLES         64
+#define MAX_MODELS              512	// these are sent over the net as bytes
+#define MAX_VWEP_MODELS         32	// could be increased to 256
+#define MAX_SOUNDS              256	// so they cannot be blindly increased
 
-#define	SAVEGAME_COMMENT_LENGTH	39
+#define	SAVEGAME_COMMENT_LENGTH 39
 
 //============================================================================
 
 // stats are integers communicated to the client by the server
 #define	MAX_CL_STATS		32
-#define	STAT_HEALTH			0
-//define STAT_FRAGS			1
-#define	STAT_WEAPON			2
-#define	STAT_AMMO			3
-#define	STAT_ARMOR			4
-//define STAT_WEAPONFRAME	5
-#define	STAT_SHELLS			6
-#define	STAT_NAILS			7
-#define	STAT_ROCKETS		8
-#define	STAT_CELLS			9
-#define	STAT_ACTIVEWEAPON	10
-#define	STAT_TOTALSECRETS	11
-#define	STAT_TOTALMONSTERS	12
-#define	STAT_SECRETS		13		// bumped on client side by svc_foundsecret
-#define	STAT_MONSTERS		14		// bumped by svc_killedmonster
-#define	STAT_ITEMS			15
-#define STAT_VIEWHEIGHT		16		// Z_EXT_VIEWHEIGHT protocol extension
-#define STAT_TIME			17		// Z_EXT_TIME extension
+
+#define	STAT_HEALTH             0
+//define STAT_FRAGS             1
+#define	STAT_WEAPON             2
+#define	STAT_AMMO               3
+#define	STAT_ARMOR              4
+//define STAT_WEAPONFRAME       5
+#define	STAT_SHELLS             6
+#define	STAT_NAILS              7
+#define	STAT_ROCKETS            8
+#define	STAT_CELLS              9
+#define	STAT_ACTIVEWEAPON       10
+#define	STAT_TOTALSECRETS       11
+#define	STAT_TOTALMONSTERS      12
+#define	STAT_SECRETS            13		// bumped on client side by svc_foundsecret
+#define	STAT_MONSTERS           14		// bumped by svc_killedmonster
+#define	STAT_ITEMS              15
+#define STAT_VIEWHEIGHT         16		// Z_EXT_VIEWHEIGHT protocol extension
+#define STAT_TIME               17		// Z_EXT_TIME extension
+#define STAT_MATCHSTARTTIME     18		// Server should send this as msec (int)
 
 // item flags
-#define	IT_SHOTGUN			1
-#define	IT_SUPER_SHOTGUN	2
-#define	IT_NAILGUN			4
-#define	IT_SUPER_NAILGUN	8
-#define	IT_GRENADE_LAUNCHER	16
-#define	IT_ROCKET_LAUNCHER	32
-#define	IT_LIGHTNING		64
-#define	IT_SUPER_LIGHTNING	128
+#define	IT_SHOTGUN              1
+#define	IT_SUPER_SHOTGUN        2
+#define	IT_NAILGUN              4
+#define	IT_SUPER_NAILGUN        8
+#define	IT_GRENADE_LAUNCHER     16
+#define	IT_ROCKET_LAUNCHER      32
+#define	IT_LIGHTNING            64
+#define	IT_SUPER_LIGHTNING      128
+#define	IT_SHELLS               256
+#define	IT_NAILS                512
+#define	IT_ROCKETS              1024
+#define	IT_CELLS                2048
+#define	IT_AXE                  4096
+#define	IT_ARMOR1               8192
+#define	IT_ARMOR2               16384
+#define	IT_ARMOR3               32768
+#define	IT_SUPERHEALTH          65536
+#define	IT_KEY1                 131072
+#define	IT_KEY2                 262144
+#define	IT_INVISIBILITY         524288
+#define	IT_INVULNERABILITY      1048576
+#define	IT_SUIT                 2097152
+#define	IT_QUAD                 4194304
 
-#define	IT_SHELLS			256
-#define	IT_NAILS			512
-#define	IT_ROCKETS			1024
-#define	IT_CELLS			2048
-
-#define	IT_AXE				4096
-
-#define	IT_ARMOR1			8192
-#define	IT_ARMOR2			16384
-#define	IT_ARMOR3			32768
-
-#define	IT_SUPERHEALTH		65536
-
-#define	IT_KEY1				131072
-#define	IT_KEY2				262144
-
-#define	IT_INVISIBILITY		524288
-
-#define	IT_INVULNERABILITY	1048576
-#define	IT_SUIT				2097152
-#define	IT_QUAD				4194304
-
-#define	IT_SIGIL1			(1<<28)
-
-#define	IT_SIGIL2			(1<<29)
-#define	IT_SIGIL3			(1<<30)
-#define	IT_SIGIL4			(1<<31)
+#define	IT_SIGIL1               (1<<28)
+#define	IT_SIGIL2               (1<<29)
+#define	IT_SIGIL3               (1<<30)
+#define	IT_SIGIL4               (1<<31)
 
 //
 // entity effects
