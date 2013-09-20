e8eef2d672c54e8b0a78f1757de8c537293b9fa6
Toni Spets <toni.spets@iki.fi>
2013-09-20 20:44:34 +0300

    Remove old build scripts and project files
    Building is now streamlined into main Makefile and it is possible
    to build both Linux and Windows binaries on Linux without much
    hassle. OSX building will be fixed later and the osx/ directory
    will be removed after sys_osx has been migrated.

--- make/utils/releases/release_2.0_beta_1.sh
+++
@@ -1,2 +0,0 @@
-make clean
-make VERSION=2.0_beta_1 SVN_EXPORT_URL=https://ezquake.svn.sourceforge.net/svnroot/ezquake/branches/ezquake_20 
--- make/utils/releases/release_2.1_beta_1.sh
+++
@@ -1,2 +0,0 @@
-make clean
-make VERSION=2.1_beta_1 TARBALL_BRANCH=ezquake_21
