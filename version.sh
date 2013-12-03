1ffaa32e350c2ae7df35655e440b798800296c0a
dimman <fog@udden.net>
2013-12-03 20:03:48 +0100

    Remove Carbon OSX files
    Minor cosmetic fixes to version.sh

--- version.sh
+++ version.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-if [ -f VERSION ]
-then
-    ver="`cat VERSION`"
-    rev="`sed -e 's/^r\([0-9]\+\).*$/\1/' VERSION`"
-elif [ -x "`which git`" -a -d ".git" ]
-then
-    rev="`git rev-list HEAD | wc -l | tr -d -c 0-9`"
-    ver="r$rev~`git rev-parse --short HEAD`"
+if [ -f VERSION ]; then
+    ver=$(cat VERSION)
+    rev=$(sed -e 's/^r\([0-9]\+\).*$/\1/' VERSION)
+elif [ -x "$(command -v git)" -a -d ".git" ]; then
+    rev=$(git rev-list HEAD | wc -l | tr -d -c 0-9)
+    ver="r$rev~$(git rev-parse --short HEAD)"
 else
     echo "WARNING: Couldn't detect ezQuake version." >&2
     ver="r666"
