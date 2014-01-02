aedd4bb0e8e66b63decacfa8fba180a1af7ab2e4
Toni Spets <toni.spets@iki.fi>
2014-01-02 18:04:48 +0200

    Improve Windows resources

--- sys_win.c
+++ sys_win.c
@@ -28,7 +28,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <direct.h>		// _mkdir
 #include <conio.h>		// _putch
 #include <tchar.h>
-#include "resource.h"
 #include "keys.h"
 #include "server.h"
 #include "pcre.h"
