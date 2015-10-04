360f4b24b4a48bea6242001faefa36f14cca3f50
Florian Zwoch <fzwoch@gmail.com>
2015-10-04 15:00:37 +0200

    remove "SDL2" include prefix. everywhere else it is without and this fixes broken osx el capitan build. it seems the default user include path is no longer exported.

--- vid.h
+++ vid.h
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 // vid.h -- video driver defs
 
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
 #define VID_CBITS 6
 #define VID_GRADES (1 << VID_CBITS)
