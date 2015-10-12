b3224cdb34e5d093d056cffd278a3901f8e6c41a
Anders Lindmark <anders.lindmark@gmail.com>
2015-10-12 16:35:08 +0200

    Instructions

--- misc/docker-nightly-build/linux.sh
+++ misc/docker-nightly-build/linux.sh
@@ -1,2 +1,1 @@
-OUTPUTDIR=/where/you/want/the/linux/binary/to/go
 docker run -v $OUTPUTDIR:/nightly localghost/ezquake_nightly sh -c "cd ezquake-source; git pull; make; cp ezquake-linux-x86_64 /nightly/; make clean"
--- misc/docker-nightly-build/nightly.sh
+++ misc/docker-nightly-build/nightly.sh
@@ -1,4 +1,1 @@
-cd $(/usr/bin/dirname $0)
-OUTPUTDIR=$(cat image_outputdir)
 docker run -v $OUTPUTDIR:/nightly localghost/ezquake_nightly
-
