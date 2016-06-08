e9bfba89ed4ccdd7b8cfe0efb8a5db383c429532
meag <meag@acm.org>
2016-06-08 22:20:55 +0100

    MENU: Add /ruleset qcon to menu

--- menu_options.c
+++ menu_options.c
@@ -219,7 +219,7 @@ const char* s_khz_enum[] = {
 #endif
 };
 const char* cl_nolerp_enum[] = {"on", "off"};
-const char* ruleset_enum[] = { "ezQuake default", "default", "Smackdown", "smackdown", "Thunderdome", "thunderdome", "Moscow TF League", "mtfl" };
+const char* ruleset_enum[] = { "ezQuake default", "default", "Smackdown", "smackdown", "Thunderdome", "thunderdome", "Moscow TF League", "mtfl", "QuakeCon", "qcon" };
 const char *mediaroot_enum[] = { "relative to exe", "relative to home", "full path" };
 const char *teamforceskins_enum[] = { "off", "use player's name", "use player's userid", "set t1, t2, t3, ..." };
 const char *enemyforceskins_enum[] = { "off", "use player's name", "use player's userid", "set e1, e2, e3, ..." };
