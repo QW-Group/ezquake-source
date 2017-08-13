67f677310834ae3d42caaaf36c0e85035e9ac21b
meag <meag@acm.org>
2017-08-13 18:29:11 +0100

    SPRITES: for() loop, vao next

--- glm_sprite.c
+++ glm_sprite.c
@@ -17,13 +17,16 @@ static GLint sprite_scale;
 static GLint sprite_origin;
 
 typedef struct glm_sprite_s {
-	float origin[3];
+	vec3_t origin;
 	float scale;
+	float texScale[2];
+	GLuint vao;
+	GLuint texture_array;
 	int texture_index;
 } glm_sprite_t;
 
 #define MAX_SPRITE_BATCH 32
-static glm_sprite_t batch[MAX_SPRITE_BATCH];
+static glm_sprite_t sprite_batch[MAX_SPRITE_BATCH];
 static int batch_count = 0;
 
 // Temporary
@@ -83,13 +86,79 @@ void GL_BeginDrawSprites(void)
 	prev_texture_array = -1;
 }
 
+void GL_FlushSpriteBatch(void)
+{
+	int i;
+	float oldMatrix[16];
+
+	glDisable(GL_CULL_FACE);
+	GL_PushMatrix(GL_MODELVIEW, oldMatrix);
+	for (i = 0; i < batch_count; ++i) {
+		glm_sprite_t* sprite = &sprite_batch[i];
+		GLuint vao = sprite->vao;
+
+		if (sprite->texture_array != prev_texture_array) {
+			glBindTexture(GL_TEXTURE_2D_ARRAY, sprite->texture_array);
+			prev_texture_array = sprite->texture_array;
+		}
+
+		GL_PopMatrix(GL_MODELVIEW, r_world_matrix);
+		GL_Translate(GL_MODELVIEW, sprite->origin[0], sprite->origin[1], sprite->origin[2]);
+		if (true)
+		{
+			float tempMatrix[16];
+
+			GL_PushMatrix(GL_MODELVIEW, tempMatrix);
+			// x = -y
+			tempMatrix[0] = 0;
+			tempMatrix[4] = -sprite->scale;
+			tempMatrix[8] = 0;
+
+			// y = z
+			tempMatrix[1] = 0;
+			tempMatrix[5] = 0;
+			tempMatrix[9] = sprite->scale;
+
+			// z = -x
+			tempMatrix[2] = -sprite->scale;
+			tempMatrix[6] = 0;
+			tempMatrix[10] = 0;
+
+			GL_PopMatrix(GL_MODELVIEW, tempMatrix);
+		}
+
+		if (spriteProgram.program && vao) {
+			float modelViewMatrix[16];
+			float projectionMatrix[16];
+
+			GLM_GetMatrix(GL_MODELVIEW, modelViewMatrix);
+			GLM_GetMatrix(GL_PROJECTION, projectionMatrix);
+
+			GL_UseProgram(spriteProgram.program);
+			glUniformMatrix4fv(sprite_modelViewMatrixUniform, 1, GL_FALSE, modelViewMatrix);
+			glUniformMatrix4fv(sprite_projectionMatrixUniform, 1, GL_FALSE, projectionMatrix);
+			glUniform1i(sprite_materialTexUniform, 0);
+			glUniform1f(sprite_textureIndex, sprite->texture_index);
+			glUniform1f(sprite_texS, sprite->texScale[0]);
+			glUniform1f(sprite_texT, sprite->texScale[1]);
+			glUniform3f(sprite_origin, -sprite->origin[1], sprite->origin[2], -sprite->origin[0]);
+			glUniform1f(sprite_scale, 1);
+
+			glBindVertexArray(vao);
+			glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+		}
+	}
+	GL_PopMatrix(GL_MODELVIEW, oldMatrix);
+	glEnable(GL_CULL_FACE);
+
+	batch_count = 0;
+}
+
 void GL_EndDrawSprites(void)
 {
 	if (batch_count) {
-		// draw
+		GL_FlushSpriteBatch();
 	}
-
-	batch_count = 0;
 }
 
 static void GL_CompileSpriteProgram(void)
@@ -129,9 +198,26 @@ void GL_EndDrawEntities(void)
 
 void GLM_DrawSimpleItem(model_t* model, int texture, vec3_t origin, vec3_t angles, float scale, float scale_s, float scale_t)
 {
+	glm_sprite_t* sprite;
+
+	if (batch_count >= MAX_SPRITE_BATCH) {
+		GL_FlushSpriteBatch();
+	}
+
+	sprite = &sprite_batch[batch_count];
+	VectorCopy(origin, sprite->origin);
+	// TODO: angles
+	sprite->scale = scale;
+	sprite->texScale[0] = scale_s;
+	sprite->texScale[1] = scale_t;
+	sprite->texture_array = model->texture_arrays[0];
+	sprite->texture_index = texture;
+	sprite->vao = model->vao_simple;
+	++batch_count;
+
+	/*
 	GLuint vao = model->vao_simple;
 	float oldMatrix[16];
-	byte color[4] = { 255, 255, 255, 255 };
 
 	if (model->texture_arrays[0] != prev_texture_array) {
 		glBindTexture(GL_TEXTURE_2D_ARRAY, model->texture_arrays[0]);
@@ -144,23 +230,23 @@ void GLM_DrawSimpleItem(model_t* model, int texture, vec3_t origin, vec3_t angle
 	if (true)
 	{
 		float tempMatrix[16];
-
+	
 		GL_PushMatrix(GL_MODELVIEW, tempMatrix);
 		// x = -y
 		tempMatrix[0] = 0;
 		tempMatrix[4] = -scale;
 		tempMatrix[8] = 0;
-
+	
 		// y = z
 		tempMatrix[1] = 0;
 		tempMatrix[5] = 0;
 		tempMatrix[9] = scale;
-
+	
 		// z = -x
 		tempMatrix[2] = -scale;
 		tempMatrix[6] = 0;
 		tempMatrix[10] = 0;
-
+	
 		GL_PopMatrix(GL_MODELVIEW, tempMatrix);
 	}
 
@@ -184,9 +270,7 @@ void GLM_DrawSimpleItem(model_t* model, int texture, vec3_t origin, vec3_t angle
 		glBindVertexArray(vao);
 		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 	}
-	else {
-		GLM_DrawTexturedPoly(color, vao, 0, 4, false, true);
-	}
 
 	GL_PopMatrix(GL_MODELVIEW, oldMatrix);
+	*/
 }
