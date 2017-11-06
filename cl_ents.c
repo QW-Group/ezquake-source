44ace064d597bfe576c5830098e816d98834141d
meag <meag@acm.org>
2017-11-06 13:46:24 +0000

    ENTS: Remove /developer flag printing model list

--- cl_ents.c
+++ cl_ents.c
@@ -881,10 +881,6 @@ void CL_LinkPacketEntities(void)
 		state = &pack->entities[pnum];
 		cent = &cl_entities[state->number];
 
-		if (developer.integer) {
-			Con_Printf("%d: %s\n", pnum, cl.model_name[state->modelindex]);
-		}
-
 		// Control powerup glow for bots.
 		if (state->modelindex != cl_modelindices[mi_player] || r_powerupglow.value) 
 		{
