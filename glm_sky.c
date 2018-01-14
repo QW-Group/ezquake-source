5db7451362f9a2b43c8d2e810163c2688d3713c8
meag <meag@acm.org>
2018-01-14 13:51:07 +0000

    MODERN: bugfix, wrong texture unit for alpha sky texture

--- glm_sky.c
+++ glm_sky.c
@@ -329,7 +329,7 @@ static void GLM_DrawSkyDome(void)
 	GL_EnterRegion("SkyDome");
 	if (BuildSkyDomeProgram()) {
 		GL_BindTextureUnit(GL_TEXTURE0, GL_TEXTURE_2D, solidskytexture);
-		GL_BindTextureUnit(GL_TEXTURE0, GL_TEXTURE_2D, alphaskytexture);
+		GL_BindTextureUnit(GL_TEXTURE1, GL_TEXTURE_2D, alphaskytexture);
 
 		GL_AlphaBlendFlags(GL_BLEND_DISABLED);
 
