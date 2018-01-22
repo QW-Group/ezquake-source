d4e274fc5424bd04ee4ae644fe223a296d72dddd
meag <meag@acm.org>
2018-01-22 12:51:48 +0000

    MINOR: move code around in glm_brushmodel to split render vs vbo

--- glm_brushmodel.c
+++ glm_brushmodel.c
@@ -51,223 +51,6 @@ void GLM_CreateBrushModelProgram(void)
 	}
 }
 
-// Sets tex->next_same_size to link up all textures of common size
-int R_ChainTexturesBySize(model_t* m)
-{
-	texture_t* tx;
-	int i, j;
-	int num_sizes = 0;
-
-	// Initialise chain
-	for (i = 0; i < m->numtextures; ++i) {
-		tx = m->textures[i];
-		if (!tx || !tx->loaded) {
-			continue;
-		}
-
-		tx->next_same_size = -1;
-		tx->size_start = false;
-	}
-
-	for (i = 0; i < m->numtextures; ++i) {
-		tx = m->textures[i];
-		if (!tx || !tx->loaded || tx->next_same_size >= 0 || !tx->gl_width || !tx->gl_height || !GL_TextureReferenceIsValid(tx->gl_texturenum)) {
-			continue; // not loaded or already processed
-		}
-
-		++num_sizes;
-		tx->size_start = true;
-		for (j = i + 1; j < m->numtextures; ++j) {
-			texture_t* next = m->textures[j];
-			if (!next || !next->loaded || next->next_same_size >= 0) {
-				continue; // not loaded or already processed
-			}
-
-			if (tx->gl_width == next->gl_width && tx->gl_height == next->gl_height) {
-				tx->next_same_size = j;
-				tx = next;
-				next->next_same_size = m->numtextures;
-			}
-		}
-	}
-
-	return num_sizes;
-}
-
-// 'source' is from GLC's float[VERTEXSIZE]
-static int CopyVertToBuffer(vbo_world_vert_t* vbo_buffer, int position, float* source, int lightmap, int material, float scaleS, float scaleT, msurface_t* surf, qbool has_luma_texture)
-{
-	vbo_world_vert_t* target = vbo_buffer + position;
-
-	VectorCopy(source, target->position);
-	target->material_coords[0] = source[3];
-	target->material_coords[1] = source[4];
-	target->lightmap_coords[0] = source[5] * 65535;
-	target->lightmap_coords[1] = source[6] * 65535;
-	target->detail_coords[0] = source[7];
-	target->detail_coords[1] = source[8];
-	if (scaleS) {
-		target->material_coords[0] *= scaleS;
-	}
-	if (scaleT) {
-		target->material_coords[1] *= scaleT;
-	}
-	target->lightmap_index = lightmap;
-	target->material_index = material;
-
-	if (surf->flags & SURF_DRAWSKY) {
-		target->flags = TEXTURE_TURB_SKY;
-	}
-	else {
-		target->flags = surf->texinfo->texture->turbType & EZQ_SURFACE_TYPE;
-	}
-
-	target->flags |=
-		(surf->flags & SURF_UNDERWATER ? EZQ_SURFACE_UNDERWATER : 0) |
-		(surf->flags & SURF_DRAWFLAT_FLOOR ? EZQ_SURFACE_IS_FLOOR : 0) |
-		(has_luma_texture ? EZQ_SURFACE_HAS_LUMA : 0);
-	memcpy(target->flatcolor, &surf->texinfo->texture->flatcolor3ub, sizeof(target->flatcolor));
-
-	return position + 1;
-}
-
-int GLM_MeasureVBOSizeForBrushModel(model_t* m)
-{
-	int j, total_surf_verts = 0, total_surfaces = 0;
-
-	for (j = 0; j < m->numsurfaces; ++j) {
-		msurface_t* surf = m->surfaces + j;
-		glpoly_t* poly;
-
-		if (!(surf->flags & (SURF_DRAWTURB | SURF_DRAWSKY))) {
-			if (surf->texinfo->flags & TEX_SPECIAL) {
-				continue;
-			}
-		}
-		if (!m->textures[surf->texinfo->miptex]) {
-			continue;
-		}
-
-		for (poly = surf->polys; poly; poly = poly->next) {
-			total_surf_verts += poly->numverts;
-			++total_surfaces;
-		}
-	}
-
-	if (total_surf_verts <= 0 || total_surfaces < 1) {
-		return 0;
-	}
-
-	return (total_surf_verts);// +2 * (total_surfaces - 1));
-}
-
-// Create VBO, ordering by texture array
-int GLM_PopulateVBOForBrushModel(model_t* m, vbo_world_vert_t* vbo_buffer, int vbo_pos)
-{
-	int i, j;
-	int combinations = 0;
-	int original_pos = vbo_pos;
-
-	// Clear lightmap data, we don't use it
-	for (i = 0; i < m->numtextures; ++i) {
-		if (m->textures[i]) {
-			memset(m->textures[i]->gl_vbo_length, 0, sizeof(m->textures[i]->gl_vbo_length));
-			memset(m->textures[i]->gl_next_lightmap, 0, sizeof(m->textures[i]->gl_next_lightmap));
-			m->textures[i]->gl_first_lightmap = -1;
-			for (j = 0; j < MAX_LIGHTMAPS; ++j) {
-				m->textures[i]->gl_next_lightmap[j] = -1;
-			}
-		}
-	}
-
-	// Order vertices in the VBO by texture & lightmap
-	for (i = 0; i < m->numtextures; ++i) {
-		int lightmap = -1;
-		int length = 0;
-		int surface_count = 0;
-		int tex_vbo_start = vbo_pos;
-		qbool has_luma = false;
-
-		if (!m->textures[i]) {
-			continue;
-		}
-
-		has_luma = GL_TextureReferenceIsValid(m->textures[i]->fb_texturenum);
-		if (has_luma) {
-			Con_Printf("%s has a luma texture\n", m->textures[i]->name);
-		}
-		for (j = 0; j < m->numsurfaces; ++j) {
-			msurface_t* surf = m->surfaces + j;
-			int lightmap = surf->flags & (SURF_DRAWTURB | SURF_DRAWSKY) ? -1 : surf->lightmaptexturenum;
-			glpoly_t* poly;
-
-			if (surf->texinfo->miptex != i) {
-				continue;
-			}
-
-			// copy verts into buffer (alternate to turn fan into triangle strip)
-			for (poly = surf->polys; poly; poly = poly->next) {
-				int end_vert = 0;
-				int start_vert = 1;
-				int output = 0;
-				int material = m->textures[i]->gl_texture_index;
-				float scaleS = m->textures[i]->gl_texture_scaleS;
-				float scaleT = m->textures[i]->gl_texture_scaleT;
-
-				if (!poly->numverts) {
-					continue;
-				}
-
-				// Store position for drawing individual polys
-				poly->vbo_start = vbo_pos;
-				vbo_pos = CopyVertToBuffer(vbo_buffer, vbo_pos, poly->verts[0], lightmap, material, scaleS, scaleT, surf, has_luma);
-				++output;
-
-				start_vert = 1;
-				end_vert = poly->numverts - 1;
-
-				while (start_vert <= end_vert) {
-					vbo_pos = CopyVertToBuffer(vbo_buffer, vbo_pos, poly->verts[start_vert], lightmap, material, scaleS, scaleT, surf, has_luma);
-					++output;
-
-					if (start_vert < end_vert) {
-						vbo_pos = CopyVertToBuffer(vbo_buffer, vbo_pos, poly->verts[end_vert], lightmap, material, scaleS, scaleT, surf, has_luma);
-						++output;
-					}
-
-					++start_vert;
-					--end_vert;
-				}
-
-				length += poly->numverts;
-				++surface_count;
-			}
-		}
-	}
-
-	return vbo_pos;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 typedef struct glm_brushmodel_req_s {
 	// This is DrawElementsIndirectCmd, from OpenGL spec
 	GLuint count;           // Number of indexes to pull
@@ -278,7 +61,6 @@ typedef struct glm_brushmodel_req_s {
 
 	float mvMatrix[16];
 	float baseColor[4];
-	qbool applyLightmap;
 	qbool polygonOffset;    // whether or not to apply polygon offset before rendering
 	GLuint vbo_count;       // Number of verts to draw
 
@@ -502,6 +284,223 @@ void GLM_DrawBrushModel(model_t* model, qbool polygonOffset)
 	}
 
 	return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Sets tex->next_same_size to link up all textures of common size
+int R_ChainTexturesBySize(model_t* m)
+{
+	texture_t* tx;
+	int i, j;
+	int num_sizes = 0;
+
+	// Initialise chain
+	for (i = 0; i < m->numtextures; ++i) {
+		tx = m->textures[i];
+		if (!tx || !tx->loaded) {
+			continue;
+		}
+
+		tx->next_same_size = -1;
+		tx->size_start = false;
+	}
+
+	for (i = 0; i < m->numtextures; ++i) {
+		tx = m->textures[i];
+		if (!tx || !tx->loaded || tx->next_same_size >= 0 || !tx->gl_width || !tx->gl_height || !GL_TextureReferenceIsValid(tx->gl_texturenum)) {
+			continue; // not loaded or already processed
+		}
+
+		++num_sizes;
+		tx->size_start = true;
+		for (j = i + 1; j < m->numtextures; ++j) {
+			texture_t* next = m->textures[j];
+			if (!next || !next->loaded || next->next_same_size >= 0) {
+				continue; // not loaded or already processed
+			}
+
+			if (tx->gl_width == next->gl_width && tx->gl_height == next->gl_height) {
+				tx->next_same_size = j;
+				tx = next;
+				next->next_same_size = m->numtextures;
+			}
+		}
+	}
+
+	return num_sizes;
+}
+
+// 'source' is from GLC's float[VERTEXSIZE]
+static int CopyVertToBuffer(model_t* mod, vbo_world_vert_t* vbo_buffer, int position, float* source, int lightmap, int material, float scaleS, float scaleT, msurface_t* surf, qbool has_luma_texture)
+{
+	vbo_world_vert_t* target = vbo_buffer + position;
+
+	VectorCopy(source, target->position);
+	target->material_coords[0] = source[3];
+	target->material_coords[1] = source[4];
+	target->lightmap_coords[0] = source[5] * 65535;
+	target->lightmap_coords[1] = source[6] * 65535;
+	target->detail_coords[0] = source[7];
+	target->detail_coords[1] = source[8];
+	if (scaleS) {
+		target->material_coords[0] *= scaleS;
+	}
+	if (scaleT) {
+		target->material_coords[1] *= scaleT;
+	}
+	target->lightmap_index = lightmap;
+	target->material_index = material;
+
+	if (surf->flags & SURF_DRAWSKY) {
+		target->flags = TEXTURE_TURB_SKY;
+	}
+	else {
+		target->flags = surf->texinfo->texture->turbType & EZQ_SURFACE_TYPE;
+	}
+
+	target->flags |=
+		(surf->flags & SURF_UNDERWATER ? EZQ_SURFACE_UNDERWATER : 0) |
+		(surf->flags & SURF_DRAWFLAT_FLOOR ? EZQ_SURFACE_IS_FLOOR : 0) |
+		(has_luma_texture ? EZQ_SURFACE_HAS_LUMA : 0);
+	if (mod->isworldmodel) {
+		target->flags |= EZQ_SURFACE_DETAIL;
+	}
+	memcpy(target->flatcolor, &surf->texinfo->texture->flatcolor3ub, sizeof(target->flatcolor));
+
+	return position + 1;
+}
+
+int GLM_MeasureVBOSizeForBrushModel(model_t* m)
+{
+	int j, total_surf_verts = 0, total_surfaces = 0;
+
+	for (j = 0; j < m->numsurfaces; ++j) {
+		msurface_t* surf = m->surfaces + j;
+		glpoly_t* poly;
+
+		if (!(surf->flags & (SURF_DRAWTURB | SURF_DRAWSKY))) {
+			if (surf->texinfo->flags & TEX_SPECIAL) {
+				continue;
+			}
+		}
+		if (!m->textures[surf->texinfo->miptex]) {
+			continue;
+		}
+
+		for (poly = surf->polys; poly; poly = poly->next) {
+			total_surf_verts += poly->numverts;
+			++total_surfaces;
+		}
+	}
+
+	if (total_surf_verts <= 0 || total_surfaces < 1) {
+		return 0;
+	}
+
+	return (total_surf_verts);// +2 * (total_surfaces - 1));
+}
+
+// Create VBO, ordering by texture array
+int GLM_PopulateVBOForBrushModel(model_t* m, vbo_world_vert_t* vbo_buffer, int vbo_pos)
+{
+	int i, j;
+	int combinations = 0;
+	int original_pos = vbo_pos;
+
+	// Clear lightmap data, we don't use it
+	for (i = 0; i < m->numtextures; ++i) {
+		if (m->textures[i]) {
+			memset(m->textures[i]->gl_vbo_length, 0, sizeof(m->textures[i]->gl_vbo_length));
+			memset(m->textures[i]->gl_next_lightmap, 0, sizeof(m->textures[i]->gl_next_lightmap));
+			m->textures[i]->gl_first_lightmap = -1;
+			for (j = 0; j < MAX_LIGHTMAPS; ++j) {
+				m->textures[i]->gl_next_lightmap[j] = -1;
+			}
+		}
+	}
+
+	// Order vertices in the VBO by texture & lightmap
+	for (i = 0; i < m->numtextures; ++i) {
+		int lightmap = -1;
+		int length = 0;
+		int surface_count = 0;
+		int tex_vbo_start = vbo_pos;
+		qbool has_luma = false;
+
+		if (!m->textures[i]) {
+			continue;
+		}
+
+		has_luma = GL_TextureReferenceIsValid(m->textures[i]->fb_texturenum);
+		if (has_luma) {
+			Con_Printf("%s has a luma texture\n", m->textures[i]->name);
+		}
+		for (j = 0; j < m->numsurfaces; ++j) {
+			msurface_t* surf = m->surfaces + j;
+			int lightmap = surf->flags & (SURF_DRAWTURB | SURF_DRAWSKY) ? -1 : surf->lightmaptexturenum;
+			glpoly_t* poly;
+
+			if (surf->texinfo->miptex != i) {
+				continue;
+			}
+
+			// copy verts into buffer (alternate to turn fan into triangle strip)
+			for (poly = surf->polys; poly; poly = poly->next) {
+				int end_vert = 0;
+				int start_vert = 1;
+				int output = 0;
+				int material = m->textures[i]->gl_texture_index;
+				float scaleS = m->textures[i]->gl_texture_scaleS;
+				float scaleT = m->textures[i]->gl_texture_scaleT;
+
+				if (!poly->numverts) {
+					continue;
+				}
+
+				// Store position for drawing individual polys
+				poly->vbo_start = vbo_pos;
+				vbo_pos = CopyVertToBuffer(m, vbo_buffer, vbo_pos, poly->verts[0], lightmap, material, scaleS, scaleT, surf, has_luma);
+				++output;
+
+				start_vert = 1;
+				end_vert = poly->numverts - 1;
+
+				while (start_vert <= end_vert) {
+					vbo_pos = CopyVertToBuffer(m, vbo_buffer, vbo_pos, poly->verts[start_vert], lightmap, material, scaleS, scaleT, surf, has_luma);
+					++output;
+
+					if (start_vert < end_vert) {
+						vbo_pos = CopyVertToBuffer(m, vbo_buffer, vbo_pos, poly->verts[end_vert], lightmap, material, scaleS, scaleT, surf, has_luma);
+						++output;
+					}
+
+					++start_vert;
+					--end_vert;
+				}
+
+				length += poly->numverts;
+				++surface_count;
+			}
+		}
+	}
+
+	return vbo_pos;
 }
 
 void GLM_CreateBrushModelVAO(glm_vbo_t* instance_vbo)
