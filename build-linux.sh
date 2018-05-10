ddf1f17ec498ae085ffe84fbdfc94cf7cb440fd3
ciscon <dustin.glidden@gmail.com>
2018-05-10 17:49:22 -0400

    BUILD: Updated linux build script to run without root privileges, instead use sudo with relevent commands.

--- build-linux.sh
+++ build-linux.sh
@@ -14,74 +14,78 @@ if [ -e "$buildlog" ];then
 	rm -f "$buildlog"
 fi
 
-debian=0
-
 #ansi color codes
 RED='\e[31m'
 GREEN='\e[32m'
 NC='\e[0m'
 
-trap "if [ \$? -ne 0 ];then echo -e \"${RED}Failure, bailing out.${NC}\";if [ -e $buildlog ];then echo \"Please see $buildlog for details.\";fi;exit 0;fi" INT TERM EXIT
+CPU=$(uname -m | sed -e s/i.86/i386/ -e s/amd64/x86_64/ -e s/sun4u/sparc64/ -e s/arm.*/arm/ -e s/sa110/arm/ -e s/alpha/axp/)
 
-if [ $(id -u) -ne 0 ];then
-	echo "You must be root to run this script as it interfaces with your package manager, run with sudo or root privileges."
-	exit 1
-fi
+trap "if [ \$? -ne 0 ];then echo -e \"${RED}Failure, bailing out.${NC}\";if [ -e $buildlog ];then echo \"Please see $buildlog for details.\";fi;exit 0;fi" INT TERM EXIT
 
 if [ ! -e Makefile ];then
 	echo "Cannot find Makefile, please run this script from the source code directory."
 	exit 1
 fi
 
+if ! type "sudo" > /dev/null; then
+	echo "Could not find sudo, please install it.  Exiting."
+	exit 1
+fi
+
+
 function check_status() {
 	if [ $? -ne 0 ];then
-		echo "Step failed."
+		cat "$buildlog"
+		echo -e "${RED}$1 - Step failed.${NC}"
 		exit 2 
 	fi
 }
 
-if [ -e /etc/debian_version ];then
+if [ -f /etc/os-release ];then
+	. /etc/os-release
+else
+	echo "Your environment isn't supported by this script.  Bailing out."
+	exit 1
+fi
+
+debian=0
+
+if [ "$ID" == "debian" ] || [ "$ID_LIKE" == "debian" ];then
 	debian=1
-	old=$(grep -e 'stretch' -e '8.[0-9]' -c '/etc/debian_version')
-elif [ ! -e /etc/redhat-release ];then
+elif [ "$ID" != "fedora" ];then
 	echo "Your environment isn't supported by this script.  Bailing out."
 	exit 1
 fi
 
 if [ $debian -eq 1 ];then
-
-	echo "Updating apt repo list..."
-	apt-get update -y -qq
-	check_status
-
-	if [ $old -eq 1 ];then
+	echo "Updating apt repo list... (running with sudo)"
+	sudo apt-get update -y -qq
+	check_status "Repo update"
+	if [ $(grep -e 'stretch' -e '8.[0-9]' -c '/etc/debian_version') -eq 1 ];then
 		packages="$deb_old"
 	else
 		packages="$deb"
 	fi
-
-	echo "Installing packages..."
-	apt-get install -y -q $packages > "$buildlog" 2>&1
-	check_status
-
+	echo "Installing packages... (running with sudo)"
+	sudo apt-get install -y -q $packages > "$buildlog" 2>&1
+	check_status "Dependency installation"
 else
-
-	echo "Updating yum repo list..."
-	yum clean all -yqqq
-	yum check-update -yqqq > /dev/null 2>&1
-	check_status
+	echo "Updating yum repo list... (running with sudo)"
+	sudo yum clean all -yqqq
+	sudo yum check-update -yqqq > "$buildlog" 2>&1
+	check_status "Repo update"
 	echo "Installing packages..."
 	packages="$rpm"
-	yum install -yq $packages > "$buildlog" 2>&1
-	check_status
-
+	sudo yum install -yq $packages >> "$buildlog" 2>&1
+	check_status "Dependency installation"
 fi
 
 echo "Cleaning up any previous build files..."
-make clean >/dev/null 2>&1
+make clean >> "$buildlog" 2>&1
 echo "Building source..."
 make -j$(nproc) >> "$buildlog" 2>&1
-check_status
-echo -e "${GREEN}Build completed successfully.${NC}"
+check_status "Compilation"
+echo -e "${GREEN}Build completed successfully.  Copy ezquake-linux-${CPU} into your quake directory.${NC}"
 
 exit 0
