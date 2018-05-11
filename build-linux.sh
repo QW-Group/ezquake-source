98f42e7ccead0b063a73590f21fe3727f7ac1957
dimman <fog@udden.net>
2018-05-11 21:47:40 +0200

    BUILD: Refactor the build-linux.sh script
    Thanks to ciscon for helping out

--- build-linux.sh
+++ build-linux.sh
@@ -1,91 +1,119 @@
-#!/bin/bash 
+#!/bin/sh -e
 # simple build script for linux
 
-#package list for debian 9.x+
-deb="git build-essential libsdl2-2.0-0 libsdl2-dev libjansson-dev libexpat1-dev libcurl4-openssl-dev libpng-dev libjpeg-dev libspeex-dev libspeexdsp-dev"
-#package list for debian 8.x
-deb_old="git build-essential libsdl2-2.0-0 libsdl2-dev libjansson-dev libexpat1-dev libcurl4-openssl-dev libpng12-dev libjpeg-dev libspeex-dev libspeexdsp-dev"
-#package list for fedora/el
-rpm="vim-common pcre-devel mesa-libGL-devel SDL2-devel make gcc jansson-devel expat-devel libcurl-devel libpng-devel libjpeg-turbo-devel speex-devel speexdsp-devel"
-
-buildlog="/tmp/ezquake-build.log"
-
-if [ -e "$buildlog" ];then
-	rm -f "$buildlog"
-fi
-
-#ansi color codes
+# ANSI color codes
 RED='\e[31m'
 GREEN='\e[32m'
+YELLOW='\e[33m'
 NC='\e[0m'
+
+BUILD_LOG=/tmp/ezquake-build.log
+
+PKGS_DEB="git build-essential libsdl2-2.0-0 libsdl2-dev libjansson-dev libexpat1-dev libcurl4-openssl-dev libpng-dev libjpeg-dev libspeex-dev libspeexdsp-dev"
+PKGS_RPM="pcre-devel mesa-libGL-devel SDL2-devel make gcc jansson-devel expat-devel libcurl-devel libpng-devel libjpeg-turbo-devel speex-devel speexdsp-devel"
+PKGS_ARCH=""
 
 CPU=$(uname -m | sed -e s/i.86/i386/ -e s/amd64/x86_64/ -e s/sun4u/sparc64/ -e s/arm.*/arm/ -e s/sa110/arm/ -e s/alpha/axp/)
 
-trap "if [ \$? -ne 0 ];then echo -e \"${RED}Failure, bailing out.${NC}\";if [ -e $buildlog ];then echo \"Please see $buildlog for details.\";fi;exit 0;fi" INT TERM EXIT
-
-if [ ! -e Makefile ];then
-	echo "Cannot find Makefile, please run this script from the source code directory."
+error() {
+	[ ! -e $BUILD_LOG ] || cat $BUILD_LOG
+	printf "${RED}[ERROR]${NC} %s\n" "$*"
 	exit 1
-fi
-
-if ! type "sudo" > /dev/null; then
-	echo "Could not find sudo, please install it.  Exiting."
-	exit 1
-fi
-
-
-function check_status() {
-	if [ $? -ne 0 ];then
-		cat "$buildlog"
-		echo -e "${RED}$1 - Step failed.${NC}"
-		exit 2 
-	fi
 }
 
-if [ -f /etc/os-release ];then
-	. /etc/os-release
+step() {
+	printf "${GREEN}[STEP ]${NC} %s\n" "$*"
+}
+
+info() {
+	printf "[INFO ] %s\n" "$*"
+}
+
+warn() {
+	printf "${YELLOW}[WARN ]${NC} %s\n" "$*"
+}
+
+install_check_deb() {
+	step "Install/check dependecies (packages)..."
+	info "You might be prompted to input your password as superuser privileges are required."
+
+	info "Updating apt repo list... (running with sudo)"
+	sudo apt-get update -y -qq || error "Failed to update package sources. Exiting."
+	info "Checking/installing required packages... (running with sudo)"
+	sudo apt-get install -y -q $PKGS_DEB >>$BUILD_LOG 2>&1 || error "Failed to install required packages. Exiting."
+}
+
+install_check_rpm() {
+	step "Install/check dependecies (packages)..."
+	info "You might be prompted to input your password as superuser privileges are required."
+	info "Updating yum repo list... (running with sudo)"
+	sudo yum clean all -yqqq && sudo yum check-update -yqqq >>$BUILD_LOG 2>&1 || error "Failed to update repo list. Exiting."
+	info "Checking/installing required packages... (running with sudo)"
+	sudo yum install -yq $PKGS_RPM >>$BUILD_LOG 2>&1 || error "Failed to install required packages. Exiting."
+}
+
+install_check_arch() {
+	error "NOT IMPLEMENTED YET. Feel free to put a pull request on https://github.com/ezQuake/ezquake-source with arch packages"
+	step "Install/check dependecies (packages)..."
+	info "You might be prompted to input your password as superuser privileges are required."
+	sudo pacman -Sy >>$BUILD_LOG 2>&1 || error "Failed to update repository cache. Exiting."
+	sudo pacman -S $PKGS_ARCH >>$BUILD_LOG 2>&1 || error "Failed to install required packages. Exiting."
+}
+
+
+if [ -f $BUILD_LOG ];then
+	rm -f $BUILD_LOG ||:
+fi
+
+[ -e Makefile ] || error "Cannot find 'Makefile', please run this script from the source code directory."
+command -v sudo >/dev/null 2>&1 || error "Could not find sudo, please install it. Exiting."
+
+
+if [ -f /etc/os-release ]; then
+	. /etc/os-release || error "Failed to source os-release file"
 else
-	echo "Your environment isn't supported by this script.  Bailing out."
-	exit 1
+	error "Your environment isn't supported by this script. Exiting."
 fi
 
-debian=0
+[ -n "${ID}" ] || error "Your dist does not specify ID in /etc/os-release. Exiting."
+[ -n "${VERSION_ID}" ] || VERSION_ID=0
+case $ID in
+	arch)
+		# FIXME: Not implemented yet (package list missing)
+		install_check_arch
+		;;
+	linuxmint)
+		[ $VERSION_ID -ge 18 ] || error "Your Linux Mint version '$VERSION_ID' is too old. Exiting."
+		install_check_deb
+		;;
+	ubuntu)
+		VERSION_ID=${VERSION_ID%.*}
+		[ $VERSION_ID -ge 16 ] || error "Your Ubuntu version '$VERSION_ID' is too old. Exiting."
+		install_check_deb
+		;;
+	debian)
+		if [ $VERSION_ID -gt 0 ] && [ $VERSION_ID -lt 8 ]; then
+			error "Your Debian version '$VERSION_ID' is too old. Exiting."
+		fi
+		# Includes Debian testing/unstable as they don't provide a VERSION_ID
+		install_check_deb
+		;;
+	centos|rhel|fedora)
+		# FIXME: Versions checks?
+		install_check_rpm
+		;;
+	*)
+		error "Your dist '$ID' isn't supported by this script. Exiting."
+		;;
+esac
 
-if [ "$ID" == "debian" ] || [ "$ID_LIKE" == "debian" ];then
-	debian=1
-elif [ "$ID" != "fedora" ];then
-	echo "Your environment isn't supported by this script.  Bailing out."
-	exit 1
-fi
+step "Cleaning up any previous build files..."
+make clean >>$BUILD_LOG 2>&1 || error "Failed to cleanup old build files"
 
-if [ $debian -eq 1 ];then
-	echo "Updating apt repo list... (running with sudo)"
-	sudo apt-get update -y -qq
-	check_status "Repo update"
-	if [ $(grep -e 'stretch' -e '8.[0-9]' -c '/etc/debian_version') -eq 1 ];then
-		packages="$deb_old"
-	else
-		packages="$deb"
-	fi
-	echo "Installing packages... (running with sudo)"
-	sudo apt-get install -y -q $packages > "$buildlog" 2>&1
-	check_status "Dependency installation"
-else
-	echo "Updating yum repo list... (running with sudo)"
-	sudo yum clean all -yqqq
-	sudo yum check-update -yqqq > "$buildlog" 2>&1
-	check_status "Repo update"
-	echo "Installing packages..."
-	packages="$rpm"
-	sudo yum install -yq $packages >> "$buildlog" 2>&1
-	check_status "Dependency installation"
-fi
+step "Compiling sources (this might take a while, please wait)..."
+njobs=2
+! command -v nproc >/dev/null 2>&1 || njobs=$(($(nproc) + 1))
+make -j$njobs >>$BUILD_LOG 2>&1 || error "Compilation failed. Exiting."
 
-echo "Cleaning up any previous build files..."
-make clean >> "$buildlog" 2>&1
-echo "Building source..."
-make -j$(nproc) >> "$buildlog" 2>&1
-check_status "Compilation"
-echo -e "${GREEN}Build completed successfully.  Copy ezquake-linux-${CPU} into your quake directory.${NC}"
-
-exit 0
+printf "\n${GREEN}Build completed successfully.${NC}\n"
+printf "Copy ${YELLOW}ezquake-linux-${CPU}${NC} into your quake directory.\n\n"
