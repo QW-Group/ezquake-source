09b8aad70e5053b55e1925c32a03cd027171a43d
meag <meag@acm.org>
2020-03-20 20:11:32 +0000

    VERSION: 3.5-alpha23.1

--- version.h
+++ version.h
@@ -70,7 +70,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 // Note: for server mods to detect version, change VERSION_NUM below
-#define VERSION_NUMBER "3.5-dev-alpha23"
+#define VERSION_NUMBER "3.5-dev-alpha23.1"
 
 void CL_Version_f(void);
 char *VersionString(void);
