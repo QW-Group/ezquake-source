678886e726a177cb09d20e2c1567c99c6bb8b6d5
Joseph Pecoraro <joepeck02@gmail.com>
2020-10-24 00:34:52 -0700

    Fix smartspawn to not affect attach while spectator

--- cl_input.c
+++ cl_input.c
@@ -883,7 +883,7 @@ void CL_FinishMove(usercmd_t *cmd)
 
 	// figure button bits
 	if ( in_attack.state & 3 ) {
-		if (cl_smartspawn.integer && (cl.stats[STAT_HEALTH] <= 0)) {
+		if (cl_smartspawn.integer && !cl.spectator && (cl.stats[STAT_HEALTH] <= 0)) {
 			// Treat +attack as +jump while player is dead with cl_smartspawn
 			// and immediately -attack to prevent shooting.
 			cmd->buttons |= BUTTON_JUMP;
