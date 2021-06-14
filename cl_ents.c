26cd1338cb3caac423360fee9c71a1276423bb64
Reki <spiper212@gmail.com>
2021-06-14 12:03:46 -0400

    cl_predict_players : readded 20ms prediction

--- cl_ents.c
+++ cl_ents.c
@@ -1682,7 +1682,7 @@ static qbool CL_AddVWepModel (entity_t *ent, int vw_index, int old_vw_frame)
 static double CL_PlayerTime (void)
 {
 	double current_time = (cls.demoplayback && !cls.mvdplayback) ? cls.demotime : cls.realtime;
-	double playertime = current_time - cls.latency;
+	double playertime = current_time - cls.latency + 0.02;
 
 	return min(playertime, current_time);
 }
