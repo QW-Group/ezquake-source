QW-Group/ezquake-source

commit 9e05ebd6be001719f1182b37cf2ef05e621e2170

RULESETS: Disable log during match in competitive rulesets
Smackdown/Thunderdome/Qcon

console.c
@@ -38,6 +38,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "localtime.h"
 #include "irc.h"
 #include "fonts.h"
+#include "rulesets.h"
 
 #define     MINIMUM_CONBUFSIZE     (1 << 15)
 #define     DEFAULT_CONBUFSIZE     (1 << 16)
@@ -563,7 +564,7 @@ void Con_PrintW(wchar *txt)
 	wchar *s;
 	static int cr;
 
-	if (!(Print_flags[Print_current] & PR_LOG_SKIP)) {
+	if (Ruleset_CanLogConsole() && !(Print_flags[Print_current] & PR_LOG_SKIP)) {
 		if (qconsole_log) {
 			char *tempbuf = Q_wcs2str_malloc(txt);
 			fprintf(qconsole_log, "%s", tempbuf);

rulesets.c
@@ -28,9 +28,21 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 static void Rulesets_OnChange_ruleset (cvar_t *var, char *value, qbool *cancel);
 
+typedef struct rulesetDef_s {
+	ruleset_t ruleset;
+	float maxfps;
+	qbool restrictTriggers;
+	qbool restrictPacket;
+	qbool restrictParticles;
+	qbool restrictSound;
+	qbool restrictLogging;
+} rulesetDef_t;
+
 static rulesetDef_t rulesetDef = {
 	rs_default,
 	72.0,
+	false,
+	false,
 	false,
 	false,
 	false
@@ -209,6 +221,7 @@ static void Rulesets_Smackdown(qbool enable)
 		rulesetDef.restrictTriggers = true;
 		rulesetDef.restrictPacket = true; // packet command could have been exploited for external timers
 		rulesetDef.restrictParticles = true;
+		rulesetDef.restrictLogging = true;
 		rulesetDef.ruleset = rs_smackdown;
 	} else {
 		for (i = 0; i < (sizeof(disabled_cvars) / sizeof(disabled_cvars[0])); i++)
@@ -221,6 +234,7 @@ static void Rulesets_Smackdown(qbool enable)
 		rulesetDef.restrictTriggers = false;
 		rulesetDef.restrictPacket = false;
 		rulesetDef.restrictParticles = false;
+		rulesetDef.restrictLogging = false;
 		rulesetDef.ruleset = rs_default;
 	}
 }
@@ -260,6 +274,7 @@ static void Rulesets_Qcon(qbool enable)
 		rulesetDef.restrictPacket = true; // packet command could have been exploited for external timers
 		rulesetDef.restrictParticles = true;
 		rulesetDef.restrictSound = true;
+		rulesetDef.restrictLogging = true;
 		rulesetDef.ruleset = rs_qcon;
 
 		MP3_Shutdown ();
@@ -275,6 +290,7 @@ static void Rulesets_Qcon(qbool enable)
 		rulesetDef.restrictPacket = false;
 		rulesetDef.restrictParticles = false;
 		rulesetDef.restrictSound = false;
+		rulesetDef.restrictLogging = false;
 		rulesetDef.ruleset = rs_default;
 	}
 }
@@ -310,6 +326,7 @@ static void Rulesets_Thunderdome(qbool enable)
 		rulesetDef.restrictTriggers = true;
 		rulesetDef.restrictPacket = true; // packet command could have been exploited for external timers
 		rulesetDef.restrictParticles = false;
+		rulesetDef.restrictLogging = true;
 		rulesetDef.ruleset = rs_thunderdome;
 	} else {
 		for (i = 0; i < (sizeof(disabled_cvars) / sizeof(disabled_cvars[0])); i++)
@@ -322,6 +339,7 @@ static void Rulesets_Thunderdome(qbool enable)
 		rulesetDef.restrictTriggers = false;
 		rulesetDef.restrictPacket = false;
 		rulesetDef.restrictParticles = false;
+		rulesetDef.restrictLogging = false;
 		rulesetDef.ruleset = rs_default;
 	}
 }
@@ -722,4 +740,9 @@ qbool Ruleset_AllowPowerupShell(model_t* model)
 	extern cvar_t gl_powerupshells;
 
 	return (bound(0, gl_powerupshells.value, 1) && ((cls.demoplayback || cl.spectator) || model->modhint != MOD_EYES));
+}
+
+qbool Ruleset_CanLogConsole(void)
+{
+	return cls.demoplayback || cls.state != ca_active || cl.standby || cl.countdown || !rulesetDef.restrictLogging;
 }

rulesets.h
@@ -44,15 +44,6 @@ typedef enum {
 	rs_mtfl
 } ruleset_t;
 
-typedef struct rulesetDef_s {
-	ruleset_t ruleset;
-	float maxfps;
-	qbool restrictTriggers;
-	qbool restrictPacket;
-	qbool restrictParticles;
-	qbool restrictSound;
-} rulesetDef_t;
-
 void  Rulesets_Init(void);
 const char* Rulesets_Ruleset(void);
 qbool Rulesets_AllowTimerefresh(void);
@@ -83,3 +74,5 @@ void Rulesets_OnChange_cl_iDrive(cvar_t *var, char *value, qbool *cancel);
 qbool Rulesets_ToggleWhenFlashed(void);
 qbool Rulesets_FullbrightModel(struct model_s* model, qbool local_singleplayer_game);
 const char* Ruleset_BlockPlayerCountMacros(void);
+
+qbool Ruleset_CanLogConsole(void);

commit 28ba902fb6c795bd02b7d89929b9bf1898f008ea

LOGGING: Fix match logs in competitive rulesets
Caused by https://github.com/ezQuake/ezquake-source/commit/9e05ebd6be001719f1182b37cf2ef05e621e2170#diff-30e0b0c63d44b7571412ebe2b28cef8f96c61f4dcd032950982d7bf985ad4a2c
Updated release notes as that one seems to have slipped by

console.c
@@ -564,15 +564,15 @@ void Con_PrintW(wchar *txt)
 	wchar *s;
 	static int cr;
 
-	if (Ruleset_CanLogConsole() && !(Print_flags[Print_current] & PR_LOG_SKIP)) {
-		if (qconsole_log) {
+	if (!(Print_flags[Print_current] & PR_LOG_SKIP)) {
+		if (Ruleset_CanLogConsole() && qconsole_log) {
 			char *tempbuf = Q_wcs2str_malloc(txt);
 			fprintf(qconsole_log, "%s", tempbuf);
 			fflush(qconsole_log);
 			Q_free(tempbuf);
 		}
 		if (Log_IsLogging()) {
-			if (log_readable.value) {
+			if (log_readable.integer) {
 				char *s, *tempbuf = Q_wcs2str_malloc(txt);
 				for (s = tempbuf; *s; s++)
 					*s = readableChars[(unsigned char)*s];