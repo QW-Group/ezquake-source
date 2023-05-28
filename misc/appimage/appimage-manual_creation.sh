aa57bf987cf16d364336ad3d4527a1c33cf3aa71
ciscon <ciscon@nicotinelounge.com>
2023-05-28 13:45:25 -0400

    PIPELINE: appimage - allow execution with different host libc

--- misc/appimage/appimage-manual_creation.sh
+++ misc/appimage/appimage-manual_creation.sh
@@ -27,11 +27,25 @@ Terminal=false
 Categories=Game;
 Keywords=quake;first;person;shooter;multiplayer;'
 
+TESTPROGRAM='
+int main(){
+ return 0;
+}
+'
+
 QUAKE_SCRIPT='#!/usr/bin/env bash
-export LD_LIBRARY_PATH="${APPIMAGE_LIBRARY_PATH}:${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${APPIMAGE_LIBRARY_PATH}:${APPDIR}/usr/lib"
 cd "$OWD"
-exec "${APPDIR}/usr/bin/ezquake-linux-'$ARCH'" $*'
-#exec "${APPDIR}/usr/lib/ld-linux-'$ARCHDASH'.so.2" "${APPDIR}/usr/bin/ezquake-linux-'$ARCH'" $*'
+"${APPDIR}/usr/bin/test"  >/dev/null 2>&1 |:
+FAIL=${PIPESTATUS[0]}
+if [ $FAIL -eq 0 ];then
+	echo "executing with native libc"
+	exec "${APPDIR}/usr/bin/ezquake-linux-'$ARCH'" $*
+else
+	echo "executing with appimage libc"
+	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${APPDIR}/usr/lib-override"
+	exec "${APPDIR}/usr/lib-override/ld-linux-'$ARCHDASH'.so.2" "${APPDIR}/usr/bin/ezquake-linux-'$ARCH'" $*
+fi'
 
 unset CC
 if [ "$ARCH" == "x86_64" ];then
@@ -45,9 +59,13 @@ DIR="$(pwd)"
 if [ -d AppDir ];then
 	rm -rf AppDir
 fi
-mkdir -p "$DIR/build" || exit 1
 mkdir -p "$DIR/AppDir/usr/bin" || exit 1
 mkdir -p "$DIR/AppDir/usr/lib" || exit 1
+mkdir -p "$DIR/AppDir/usr/lib-override" || exit 1
+
+echo "$TESTPROGRAM" > "$DIR/test.c" || exit 2
+gcc "$DIR/test.c" -o "$DIR/AppDir/usr/bin/test" ||exit 2
+rm -f "$DIR/AppDir/test.c" || exit 2
 
 VERSION=$(sed -n 's/.*VERSION_NUMBER.*"\(.*\)".*/\1/p' src/version.h)
 REVISION=$(git log -n 1|head -1|awk '{print $2}'|cut -c1-6)
@@ -69,11 +87,12 @@ mkdir -p "$DIR/AppDir/usr/share/metainfo"
 sed 's,EZQUAKE_VERSION,'$VERSION-$REVISION',g;s,EZQUAKE_DATE,'$(date +%F)',g' "$DIR/misc/appimage/ezquake.appdata.xml.template" > "$DIR/AppDir/usr/share/metainfo/ezquake.appdata.xml"
 ldd "$DIR/AppDir/usr/bin/ezquake-linux-$ARCH" | \
 	grep --color=never -v libGL| \
-	grep --color=never -v libc.so| \
 	awk '{print $3}'| \
 	xargs -I% cp -Lf "%" "$DIR/AppDir/usr/lib/." || exit 5
 strip -s "$DIR/AppDir/usr/lib/"* || exit 5
 strip -s "$DIR/AppDir/usr/bin/"* || exit 5
+mv -f "$DIR/AppDir/usr/lib/libc.so.6" "$DIR/AppDir/usr/lib-override/."
+cp -Lf "/lib64/ld-linux-${ARCHDASH}.so.2" "$DIR/AppDir/usr/lib-override/." || exit 6
 
 cd "$DIR" || exit 5
 ./appimagetool AppDir ezQuake-$ARCH.AppImage || exit 7
