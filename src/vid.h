59248f9de9701f790b4298194730ac403fd4cd43
Daniel Svensson <dsvensson@gmail.com>
2024-03-20 02:01:18 +0100

    BUILD: Avoid strlen conflict with SDL2.

--- src/vid.h
+++ src/vid.h
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef EZQUAKE_VID_HEADER
 #define EZQUAKE_VID_HEADER
 
+#undef strlen
 #include <SDL.h>
 
 #define VID_CBITS 6
