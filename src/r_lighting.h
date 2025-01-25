9adb697389b6ff33c856b7623299967e442aaa08
Daniel Svensson <dsvensson@gmail.com>
2025-01-25 02:05:32 +0100

    LIGHTMAPS: Support for loading e5bgr9 lightmaps.
    This format was added to ericw-tools spring 2024 and initial implementation
    does not expose it's full range potential, but it loads, and works, which allows
    mappers to target this format instead of the old rgb8 format.
    
    Ported from QuakeSpasm-Spiked.

--- src/r_lighting.h
+++ src/r_lighting.h
@@ -34,6 +34,14 @@ void R_RenderDlights(void);
 void R_LightEntity(entity_t* ent);
 
 extern unsigned int d_lightstylevalue[256];	// 8.8 fraction of base light value
+static const float rgb9e5tab[32] = {
+	//multipliers for the 9-bit mantissa, according to the biased mantissa
+	//aka: pow(2, biasedexponent - bias-bits) where bias is 15 and bits is 9
+	1.0/(1<<24), 1.0/(1<<23), 1.0/(1<<22), 1.0/(1<<21), 1.0/(1<<20), 1.0/(1<<19), 1.0/(1<<18), 1.0/(1<<17),
+	1.0/(1<<16), 1.0/(1<<15), 1.0/(1<<14), 1.0/(1<<13), 1.0/(1<<12), 1.0/(1<<11), 1.0/(1<<10), 1.0/(1<<9),
+	1.0/(1<<8),  1.0/(1<<7),  1.0/(1<<6),  1.0/(1<<5),  1.0/(1<<4),  1.0/(1<<3),  1.0/(1<<2),  1.0/(1<<1),
+	1.0,         1.0*(1<<1),  1.0*(1<<2),  1.0*(1<<3),  1.0*(1<<4),  1.0*(1<<5),  1.0*(1<<6),  1.0*(1<<7),
+};
 
 extern cvar_t r_dynamic;
 #define R_NoLighting()       (r_dynamic.integer == 0)
