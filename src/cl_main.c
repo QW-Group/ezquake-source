fe10a73d436614facd014cd3f82e1dbdfe1b0d42
Oscar Linderholm <osm@recv.se>
2025-04-08 20:03:03 +0200

    Enable port ping probe on reconnect

--- src/cl_main.c
+++ src/cl_main.c
@@ -1433,6 +1433,9 @@ void CL_Reconnect_f (void)
 		// switch the stuff
 		Cbuf_AddText(va("connect %s\n", cls.servername));
 	}
+	else if (IsPortPingProbeEnabled()) {
+		Cbuf_AddText(va("connect %s\n", cls.servername));
+	}
 	else {
 		// good old reconnect, no need to do anything special
 		Host_EndGame();
